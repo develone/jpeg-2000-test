360ccd45180e4193b44a22550457e6d95fb4c2f8
Edward Vidal Jr <develone@sbcglobal.net>
2014-06-09 14:37:24 -0600

    modified tb_eq_d.v to be able test with iverilog

--- test_verilog/tb_eq_d.v
+++ test_verilog/tb_eq_d.v
@@ -1,24 +1,36 @@
 module tb_eq_d;
 
+initial 
+ begin
+    $dumpfile("tb_eq_d.vcd");
+    $dumpvars(0,tb_eq_d);
+ end
+
+/* Make a regular pulsing clock. */
+  reg clk = 0;
+  always #5 clk = !clk;
+
+initial begin
+     # 17 x2 = 164;
+     # 15 x3 = 164;
+     # 14 x4 = 164;
+     # 19 x2 = 164;
+     # 17 x3 = 164;
+     # 16 x4 = 156; 
+     # 21 x2 = 108;
+     # 19 x3 = 200;
+     # 18 x4 = 254; 
+     # 100 $stop;
+end 
+  
 wire [9:0] d3;
 wire [9:0] a2;
-reg clk;
+
 reg [9:0] x2;
 reg [9:0] x3;
 reg [9:0] x4;
 
-initial begin
-    $from_myhdl(
-        clk,
-        x2,
-        x3,
-        x4
-    );
-    $to_myhdl(
-        d3,
-        a2
-    );
-end
+ 
 
 eq_d dut(
     d3,
@@ -29,4 +41,9 @@ eq_d dut(
     x4
 );
 
+initial
+     $monitor("At time %t, d3  = %h (%0d) ,a2  = %h (%0d)",
+              $time, d3, d3,a2,a2);
+
+ 
 endmodule
