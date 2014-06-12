194071c44baf953f294a67ee9fb03a5f0f2c7810
Edward Vidal <vidal@sim4.swbell.net>
2014-06-11 19:31:05 -0600

    lena_256.png now was tested with wavelet97lift

--- python_dwt/test_wavelet97lift.py
+++ python_dwt/test_wavelet97lift.py
@@ -1,6 +1,6 @@
 
 import  wavelet97lift
-im = wavelet97lift.Image.open("test1_512.png")
+im = wavelet97lift.Image.open("lena_256.png")
 pix = im.load()
 m = list(im.getdata())
 m = [m[i:i+im.size[0]] for i in range(0, len(m), im.size[0])]
@@ -10,7 +10,7 @@ for row in range(0, len(m)):
 		m[row][col] = float(m[row][col])
                 
 # Perform a forward CDF 9/7 transform on the image:
-m = wavelet97lift.fwt97_2d(m, 1)
+m = wavelet97lift.fwt97_2d(m, 3)
     
 wavelet97lift.seq_to_img(m, pix) # Convert the list of lists matrix to an image.
 im.save("test1_512_fwt.png") # Save the transformed image.
