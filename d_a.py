9219be3f7cdafe9727f03c71c7e9ea46f500629f
Edward Vidal Jr <develone@sbcglobal.net>
2014-06-12 09:39:15 -0600

    additional cleanup adding fwt97_2d_int fwt97_int

--- d_a.py
+++ d_a.py
@@ -32,20 +32,9 @@ import scipy
 import scipy.misc as sg_m
 import scipy.signal as sg
 import numpy as np
-
+#wavelet97lift needs to be 1 level below
+#PYTHONPATH /home/vidal/lib/python:./python_dwt:
 import  wavelet97lift
-
-def get_col(col_ind,img,wr_img_sz):
-	#print col_ind
-	for i in range(w_img_sz):
-		col[i] = img[i,col_ind]
-	return col
-		
-def get_row(row_id,img,w_img_sz):
-	#print row_ind
-	for j in range(w_img_sz):
-		row[j] = img[row_id,j]
-	return row	 
 
  
 def convert_hdl():
@@ -60,35 +49,6 @@ def convert_hdl():
 LPSynthesisFilter = np.array([ 0.5, 1, 0.5 ])
 HPSynthesisFilter = np.array([ -0.125, -0.25, 0.75, -0.25, -0.125 ])
  
-ar_size = (256,256)
-w_img_sz = 256
-#now img is 2-dimensional of type int
-#now row is 1-dimensional of type int
-#now col is 1-dimensional of type int
-#now rowl is 1-dimensional of type int
-img = np.zeros(ar_size,dtype=np.int) 
-img_odd = np.zeros((128,128),dtype=np.int) 
-img_even = np.zeros((128,128),dtype=np.int)
-col = np.zeros(w_img_sz,dtype=np.int)
-row = np.zeros(256,dtype=np.int)
-row_even = np.zeros(128,dtype=np.int)
-row_odd = np.zeros(128,dtype=np.int)
-col_even = np.zeros(64,dtype=np.int)
-col_odd = np.zeros(64,dtype=np.int)
-ar_size = (128,128)
-D = np.zeros(ar_size,dtype=np.int)
-A = np.zeros(ar_size,dtype=np.int)	
-#DD & AA are 128 following the downsample
-ar_size = (256,256)
-DD = np.zeros(ar_size,dtype=np.int)
-AA = np.zeros(ar_size,dtype=np.int)
-ar_size = (128,128)	
-DDD = np.zeros(ar_size,dtype=np.int)
-AAA = np.zeros(ar_size,dtype=np.int)
-ar_size = (64,64)
-DDDD = np.zeros(ar_size,dtype=np.int)
-AAAA = np.zeros(ar_size,dtype=np.int)	
-	
  
 
  
@@ -159,15 +119,121 @@ def eq_d_c2(d3,a2,clk,x2,x3,x4):
 		 
 		 
 	return eq_logic	
+def fwt97_2d_int(m, nlevels=1):
+    ''' Perform the CDF 9/7 transform on a 2D matrix signal m.
+    nlevel is the desired number of times to recursively transform the 
+    signal. '''
+    
+    w = len(m[0])
+    h = len(m)
+    for i in range(nlevels):
+        m = fwt97_int(m, w, h) # cols
+        m = fwt97_int(m, w, h) # rows
+        w /= 2
+        h /= 2
+    
+    return m
 
+def fwt97_int(s, width, height):
+    ''' Forward Cohen-Daubechies-Feauveau 9 tap / 7 tap wavelet transform   
+    performed on all columns of the 2D n*n matrix signal s via lifting.
+    The returned result is s, the modified input matrix.
+    The highpass and lowpass results are stored on the left half and right
+    half of s respectively, after the matrix is transposed. '''
+        
+    # 9/7 Coefficients:
+    a1 = -1.586134342
+    a2 = -0.05298011854
+    a3 = 0.8829110762
+    a4 = 0.4435068522
+    
+    # Scale coeff:
+    k1 = 0.81289306611596146 # 1/1.230174104914
+    k2 = 0.61508705245700002 # 1.230174104914/2
+    # Another k used by P. Getreuer is 1.1496043988602418
+        
+    for col in range(width): # Do the 1D transform on all cols:
+        ''' Core 1D lifting process in this loop. '''
+        ''' Lifting is done on the cols. '''
+        
+        # Predict 1. y1
+        for row in range(1, height-1, 2):
+            s[row][col] += a1 * (s[row-1][col] + s[row+1][col])   
+        s[height-1][col] += 2 * a1 * s[height-2][col] # Symmetric extension
+        
+        # Update 1. y0
+        for row in range(2, height, 2):
+            s[row][col] += a2 * (s[row-1][col] + s[row+1][col])
+        s[0][col] +=  2 * a2 * s[1][col] # Symmetric extension
+        
+        # Predict 2.
+        for row in range(1, height-1, 2):
+            s[row][col] += a3 * (s[row-1][col] + s[row+1][col])
+        s[height-1][col] += 2 * a3 * s[height-2][col]
+        
+        # Update 2.
+        for row in range(2, height, 2):
+            s[row][col] += a4 * (s[row-1][col] + s[row+1][col])
+        s[0][col] += 2 * a4 * s[1][col]
+                
+    # de-interleave
+    temp_bank = [[0]*width for i in range(height)]
+    for row in range(height):
+        for col in range(width):
+            # k1 and k2 scale the vals
+            # simultaneously transpose the matrix when deinterleaving
+            if row % 2 == 0: # even
+                temp_bank[col][row/2] = k1 * s[row][col]
+            else:            # odd
+                temp_bank[col][row/2 + height/2] = k2 * s[row][col]
+                
+    # write temp_bank to s:
+    for row in range(width):
+        for col in range(height):
+            s[row][col] = temp_bank[row][col]
+                
+    return s
+#im is an instance provided by PIL
+#im is 2-d im[0] & im[1] are the sizes
 #Using PIL to read image
-#im = wavelet97lift.Image.open("lena_256.png")
-im = wavelet97lift.Image.open("lena_256.pgm")
+im = wavelet97lift.Image.open("python_dwt/test1_512.png")
+#im = wavelet97lift.Image.open("lena_256.pgm")
+
+# Create an image buffer object for fast access.
 pix = im.load()
-print im.format, im.size, im.mode
+
+
+#m is a list 
+# Convert the 2d image to a 1d sequence:
 m = list(im.getdata())
+
+#m is a list 
+# Convert the 2d image to a 1d sequence:
 m = [m[i:i+im.size[0]] for i in range(0, len(m), im.size[0])]
+
 # Cast every item in the list to a float:
-for row in range(0, len(m)):
-	for col in range(0, len(m[0])):
-		m[row][col] = float(m[row][col])
+#for row in range(0, len(m)):
+#	for col in range(0, len(m[0])):
+#		m[row][col] = float(m[row][col])
+		
+# Perform a forward CDF 9/7 transform on the image:
+m = fwt97_2d_int(m, 3)		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
