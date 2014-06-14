a0151e3d1083d9a7be30deea68f5e67075e73387
Edward Vidal Jr <develone@sbcglobal.net>
2014-06-14 12:50:26 -0600

    adding the simulation of add_shift.

--- test_verilog/tb_add_shift.v
+++ test_verilog/tb_add_shift.v
@@ -1,5 +1,9 @@
 module tb_add_shift;
 
+initial begin
+    $dumpfile("tb_add_shift.vcd");
+    $dumpvars(0,tb_add_shift);
+ end
 wire [18:0] d3;
 wire [18:0] a2;
 reg [18:0] x2;
@@ -7,15 +11,17 @@ reg [18:0] x3;
 reg [18:0] x4;
 
 initial begin
-    $from_myhdl(
-        x2,
-        x3,
-        x4
-    );
-    $to_myhdl(
-        d3,
-        a2
-    );
+     # 17 x2 = 164;
+     # 17 x3 = 164;
+     # 17 x4 = 164;
+     # 20 x2 = 164;
+     # 20 x3 = 156;
+     # 20 x4 = 148;	
+     # 17 x2 = 112;
+     # 17 x3 = 132;
+     # 17 x4 = 142;
+     # 100 $stop; 
+
 end
 
 add_shift dut(
@@ -26,4 +32,7 @@ add_shift dut(
     x4
 );
 
+initial
+     $monitor("At time %t, d3  = %h (%0d) ,a2  = %h (%0d)",
+              $time, d3, d3,a2,a2);
 endmodule
