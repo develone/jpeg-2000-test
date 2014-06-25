3547e3a4202e18bd868d624a52fcad0f2d1bf1b0
Edward Vidal Jr <vidal@ws009.swbell.net>
2014-06-25 09:58:51 -0600

    Now using x2 = fixbv(s[row-1][col])[20,10] to create x2, x3, x4, and x5 not getting alignment error.

--- ipython_fixbv/wavelet97lift.py
+++ ipython_fixbv/wavelet97lift.py
@@ -90,8 +90,8 @@ def fwt97(s, width, height):
 			odd_even = bool(1)
 			p = bool(0)
 
-			x2 = fixbv(s[row-1][col], min = -DATA_WIDTH, max = DATA_WIDTH)
-			x3 = fixbv(s[row+1][col], min = -DATA_WIDTH, max = DATA_WIDTH)
+			x2 = fixbv(s[row-1][col])[20,10]
+			x3 = fixbv(s[row+1][col])[20,10]
 			x4 = fixbv(156, min = -DATA_WIDTH, max = DATA_WIDTH)
 			x5 = fixbv(156, min = -DATA_WIDTH, max = DATA_WIDTH)
 			print row, float(x2), float(x3),p,odd_even,s[row][col]
@@ -108,8 +108,8 @@ def fwt97(s, width, height):
 			p= bool(0)
 			x2 = fixbv(156, min = -DATA_WIDTH, max = DATA_WIDTH)
 			x3 = fixbv(156, min = -DATA_WIDTH, max = DATA_WIDTH)
-			x4 = fixbv(s[row-1][col], min = -DATA_WIDTH, max = DATA_WIDTH)
-			x5 = fixbv(s[row+1][col], min = -DATA_WIDTH, max = DATA_WIDTH)
+			x4 = fixbv(s[row-1][col])[20,10]
+			x5 = fixbv(s[row+1][col])[20,10]
 			print row, float(x4), float(x5),p,odd_even,s[row][col]
 			d_instance = add_mul_sim(d3,a2,x2,x3,x4,x5,p,odd_even)
 			#print row, float(d_instance[1]), float(x4), float(x5),odd_even,p
