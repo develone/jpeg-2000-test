faa086a18b6540ee51a5d05bbb3c6795be30fca0
Edward Vidal Jr <vidal@sim4.swbell.net>
2014-06-28 21:43:11 -0600

    add Signals of n

--- ipython_fixbv/multiple_add_mul.py
+++ ipython_fixbv/multiple_add_mul.py
@@ -30,11 +30,26 @@ print sig_list[0][9]
 print sig_list[0][3]
 sig_list[0][3] = Signal(fixbv(200)[ww])
 print sig_list[0][3]
-import add_mul_m
-def top_level (sig_list):
+from  add_mul_m import add_mul
+
+def top_level (d3,a2,clk,x2,x3,x4,x5,p,even_odd,fwd_inv,n=8):
 	""" Function doc """
-	instance_0 = add_mul(sig_list[0])
-	instance_1 = add_mul(sig_list[1])
-	instance_2 = add_mul(sig_list[2])
-	instance_3 = add_mul(sig_list[3])
+	d3 = (Signal(fixbv(0)[ww]) for i in range(n))
+	a2 = (Signal(fixbv(0)[ww]) for i in range(n))
+	x2 = (Signal(fixbv(156)[ww]) for i in range(n))
+	x3 = (Signal(fixbv(164)[ww]) for i in range(n))
+	x4 = (ignal(fixbv(156)[ww]) for i in range(n))
+	x5 = (Signal(fixbv(156)[ww]) for i in range(n))
+	clk = Signal(bool(0))
+	even_odd = (Signal(bool(0)) for i in range(n)) 
+	p = (Signal(bool(0)) for i in range(n))
+	fwd_inv = (Signal(bool(0)) for i in range(n))
 	
+	
+	for i in range(n):
+		inst[i] = add_mul(d3[i],a2[i],clk,x2[i],x3[i],x4[i],x5[i],p[i],even_odd[i],fwd_inv[i])
+	return  inst
+	
+a = top_level(d3,a2,clk,x2,x3,x4,x5,p,even_odd,fwd_inv,4)
+
+ 
