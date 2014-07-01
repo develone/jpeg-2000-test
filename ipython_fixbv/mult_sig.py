c0fc2357f83d746f33795393e8c3de4126aa13bf
Edward Vidal Jr <vidal@sim4.swbell.net>
2014-06-30 21:59:47 -0600

    using inter to create mult_sig

---
+++ ipython_fixbv/mult_sig.py
@@ -0,0 +1,158 @@
+#http://dev.myhdl.org/
+from myhdl import *
+#d3 d3
+#a2 a2
+ 
+#x2 x2,x2_1,x2_2,x2_3
+#x3 x3,x3_1,x3_2,x3_3
+#from prev operation
+#x4 x4,x4_1,x4_2,x4_3
+#x5 x5,x5_1,x5_2,x5_3
+class MyObj(object):
+	
+	def __init__(self):
+		ww = (32,8)
+		self.x2 = Signal(fixbv(0)[ww])
+		self.x3 = Signal(fixbv(0)[ww])
+		self.x4 = Signal(fixbv(0)[ww])
+		self.x5 = Signal(fixbv(0)[ww])
+		
+		self.x2_1 = Signal(fixbv(0)[ww])
+		self.x3_1 = Signal(fixbv(0)[ww])
+		self.x4_1 = Signal(fixbv(0)[ww])
+		self.x5_1 = Signal(fixbv(0)[ww])
+		
+		self.x2_2 = Signal(fixbv(0)[ww])
+		self.x3_2 = Signal(fixbv(0)[ww])
+		self.x4_2 = Signal(fixbv(0)[ww])
+		self.x5_2 = Signal(fixbv(0)[ww])
+
+		self.x2_3 = Signal(fixbv(0)[ww])
+		self.x3_3 = Signal(fixbv(0)[ww])
+		self.x4_3 = Signal(fixbv(0)[ww])
+		self.x5_3 = Signal(fixbv(0)[ww])
+
+		self.d3 = Signal(fixbv(0)[ww])
+		self.a2 = Signal(fixbv(0)[ww])
+		self.d3_1 = Signal(fixbv(0)[ww])
+		self.a2_1 = Signal(fixbv(0)[ww])
+		self.d3_2 = Signal(fixbv(0)[ww])
+		self.a2_2 = Signal(fixbv(0)[ww])
+		self.d3_3 = Signal(fixbv(0)[ww])
+		self.a2_3 = Signal(fixbv(0)[ww])
+		
+	"""Display variables x2,x3,x4,x5,d3, and a1"""
+	
+	def disSig_x2(self):
+		print "Signal : ", self.x2
+	def disSig_x4(self):
+		print "Signal : ", self.x4
+	def disSig_d3(self):
+		print "Signal : ", self.d3
+	def disSig_x3(self):
+		print "Signal : ", self.x3
+	def disSig_x5(self):
+		print "Signal : ", self.x5
+	def disSig_a1(self):
+		print "Signal : ", self.a1	
+				
+	"""Set variables x2,x3,x4,x5"""
+	
+	def setSig_x2(self,val): 
+		ww = (32,8)   
+		self.x2 = Signal(fixbv(val)[ww]) 
+	def setSig_y(self,val):
+		ww = (32,8)    
+		self.y = Signal(fixbv(val)[ww])
+	def setSig_x3(self,val): 
+		ww = (32,8)   
+		self.x3 = Signal(fixbv(val)[ww]) 
+	def setSig_y(self,val):
+		ww = (32,8)    
+		self.x5 = Signal(fixbv(val)[ww])		     
+def m_ex1(clk, p, even_odd, fwd_inv, pix):
+	ww = (32,8)
+	ca1 = Signal(fixbv(-1.586134342)[ww])
+	ca2 = Signal(fixbv(-0.05298011854)[ww])
+	ca3 = Signal(fixbv(0.8829110762)[ww])
+	ca4 = Signal(fixbv(0.4435068522)[ww])
+	ra1 = Signal(fixbv(1.586134342)[ww])
+	ra2 = Signal(fixbv(0.05298011854)[ww])
+	ra3 = Signal(fixbv(-0.8829110762)[ww])
+	ra4 = Signal(fixbv(-0.4435068522)[ww])
+	t = Signal(fixbv(0)[ww])
+	t1 = Signal(fixbv(0)[ww])
+	
+	@always(clk.posedge)
+	def hdl():
+		if not p:
+			if even_odd:
+				if fwd_inv:
+					pix.d3.next = (pix.x2 + pix.x3)*ca1
+					pix.d3_1.next = (pix.x2_1 + pix.x3_1)*ca1
+					pix.d3_2.next = (pix.x2_2 + pix.x3_2)*ca1
+					pix.d3_3.next = (pix.x2_2 + pix.x3_2)*ca1
+				else:
+					pix.a2.next = (pix.x4 + pix.x5)*ra4
+					pix.a2_1.next = (pix.x4_1 + pix.x5_1)*ra4
+					pix.a2_2.next = (pix.x4_2 + pix.x5_2)*ra4
+					pix.a2_3.next = (pix.x4_3 + pix.x5_3)*ra4
+			else:
+				if fwd_inv:
+					pix.a2.next = (pix.x4 + pix.x5)*ca2
+					pix.a2_1.next = (pix.x4_1 + pix.x5_1)*ca2
+					pix.a2_2.next = (pix.x4_2 + pix.x5_2)*ca2
+					pix.a2_3.next = (pix.x4_3 + pix.x5_3)*ca2
+
+				else:
+					pix.d3.next = (pix.x2 + pix.x3)*ra3	 
+					pix.d3_1.next = (pix.x2_1 + pix.x3_1)*ra3	 
+					pix.d3_2.next = (pix.x2_2 + pix.x3_2)*ra3	 
+					pix.d3_3.next = (pix.x2_2 + pix.x3_3)*ra3	 
+		else:
+			if even_odd:
+				if fwd_inv:
+					pix.d3.next = (pix.x2 + pix.x3)*ca3
+					pix.d3_1.next = (pix.x2_1 + pix.x3_1)*ca3
+					pix.d3_2.next = (pix.x2_2 + pix.x3_2)*ca3
+					pix.d3_3.next = (pix.x2_3 + pix.x3_3)*ca3
+				else:
+					pix.a2.next = (pix.x4 + pix.x5)*ra2
+					pix.a2_1.next = (pix.x4_1 + pix.x5_1)*ra2
+					pix.a2_2.next = (pix.x4_2 + pix.x5_2)*ra2
+					pix.a2_3.next = (pix.x4_3 + pix.x5_3)*ra2
+
+
+
+			else:
+		
+				if fwd_inv:
+					pix.a2.next = (pix.x4 + pix.x5)*ca4
+					pix.a2_1.next = (pix.x4_1 + pix.x5_1)*ca4
+					pix.a2_2.next = (pix.x4_2 + pix.x5_2)*ca4
+					pix.a2_3.next = (pix.x4_3 + pix.x5_3)*ca4
+				else:
+					pix.d3.next = (pix.x2 + pix.x3)*ra1
+					pix.d3_1.next = (pix.x2_1 + pix.x3_1)*ra1
+					pix.d3_2.next = (pix.x2_2 + pix.x3_2)*ra1
+					pix.d3_3.next = (pix.x2_3 + pix.x3_3)*ra1
+	
+	return hdl
+fwd_inv = Signal(False)
+even_odd = Signal(False)
+p = Signal(False)
+clk = Signal(False)
+pix = MyObj()
+pix.disSig_x2()
+pix.disSig_x3()
+
+pix.setSig_x2(3)
+pix.disSig_x2()
+
+pix.setSig_x3(2)
+pix.disSig_x3()
+
+pix.disSig_d3()
+#m_ex1(clk,pix)
+toVerilog(m_ex1, clk, p, even_odd,fwd_inv, pix)
+toVHDL(m_ex1, clk, p,even_odd, fwd_inv, pix)
