1fd5eaae48151fef1f9933e39a9edf2692474b15
Edward Vidal Jr <vidal@ws009.swbell.net>
2014-07-02 08:41:32 -0600

    making ww = (40,32) in all of mult_sig uses

--- ipython_fixbv/mult_sig.py
+++ ipython_fixbv/mult_sig.py
@@ -68,16 +68,16 @@ class MyObj(object):
 	"""Set variables x2,x3,x4,x5"""
 	
 	def setSig_x2(self,val): 
-		ww = (32,8)   
+		ww = (40,24)   
 		self.x2 = Signal(fixbv(val)[ww]) 
 	def setSig_x3(self,val):
-		ww = (32,8)    
+		ww = (40,24)    
 		self.x3 = Signal(fixbv(val)[ww])
 	def setSig_x4(self,val): 
-		ww = (32,8)   
+		ww = (40,24)   
 		self.x4 = Signal(fixbv(val)[ww]) 
 	def setSig_x5(self,val):
-		ww = (32,8)    
+		ww = (40,24)    
 		self.x5 = Signal(fixbv(val)[ww])		     
 
 def m_ex1(clk, p, even_odd, fwd_inv, pix):
