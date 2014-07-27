0ae52797ef1800f37d3db49091af862f958e051c
Edward Vidal <vidal@sim4.swbell.net>
2014-07-27 10:45:49 -0600

    adding missing to reset

--- ipython_fixbv/jpeg_utils.py
+++ ipython_fixbv/jpeg_utils.py
@@ -57,7 +57,14 @@ class Add_shift_top(object):
 
 	def reset(self):
 		duration = self.kwargs['duration']	
-			
+        print '-- Resetting --'
+        self.presetn.next = False
+        yield delay(duration * 5)
+
+        print '-- Reset --'
+        self.presetn.next = True
+        yield delay(duration * 5)
+        			
 	def setSig_we_odd(self,val):   
 		self.we_odd.next = Signal(bool(val))
 		
