03b92878769b0f646a2410868f9a673b2f7d16ef
Edward Vidal <vidal@ws009.swbell.net>
2014-08-25 14:50:00 -0600

    wr.bat saves the file "c:\Users\vidal\My Documents\GitHub\jpeg-2000-test\ipython_fixbv\lena.hex" to XulA2-LX9 RAM
    rd.bat reads the XulA2-LX9 RAM and stores the contents
    in a file m:\ttt.hex which is on a samba drive on sim3
    /home/samba

---
+++ rd.bat
@@ -0,0 +1,2 @@
+cd "c:\Program Files (x86)\XSTOOLs"
+xsload.exe -usb 0 -u 0x0 0x7FFFF -ram m:\ttt.hex
---
+++ wr.bat
@@ -0,0 +1,2 @@
+cd "c:\Program Files (x86)\XSTOOLs"
+xsload.exe -usb 0 -ram "c:\Users\vidal\My Documents\GitHub\jpeg-2000-test\ipython_fixbv\lena.hex"
