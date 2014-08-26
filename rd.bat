dcadf1e9c709d92ffd73d62b38bb623daf2d9098
Edward Vidal <vidal@ws009.swbell.net>
2014-08-26 09:15:02 -0600

    chg rd.bat

--- rd.bat
+++ rd.bat
@@ -1,2 +1,8 @@
-cd "c:\Program Files (x86)\XSTOOLs"
-xsload.exe -usb 0 -f hex -u 0x0 0x7FFFF -ram m:\ttt.hex
+m:
+dir ttt.hex
+del ttt.hex
+"c:\Program Files (x86)\XSTOOLs\xsload.exe" -usb 0 -f hex -u 0x00000 0x7FFFF -ram ttt.hex
+dir ttt.hex
+cp ttt.hex "c:\Users\vidal\Documents\GitHub\jpeg-2000-test\ipython_fixbv"
+dir ttt.hex
+pause
