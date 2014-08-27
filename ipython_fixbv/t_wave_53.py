3d7bf324eed20b1d9c7c0bc4dca3eab2d6ac9243
Edward Vidal <vidal@ws009.swbell.net>
2014-08-26 19:14:01 -0600

    now reads the hex formatted data converts to list and performs first level of dwt save a new tmp_level.bin which is used to generate lena_1.hex This file should match the results done on FPGA

--- ipython_fixbv/t_wave_53.py
+++ ipython_fixbv/t_wave_53.py
@@ -1,17 +1,43 @@
 import waveletsim_53 as dwt
+from  intelhex import *
+from struct import *
+def convert_intelhex_to_list():
+	"""Converts the 16bit to list m[row][col]
+	this is the procedure that will be needed in the FPGA"""
+
+	ih_lena = IntelHex()
+	ih_lena.loadhex('lena.hex')
+	pydict = ih_lena.todict()
+ 	i = 0
+	x = 0
+	for row in range(0, len(m[0])):
+		for col in range(0, len(m[1])):
+			#if col == 0:
+				#print col, row , i
+			"""creating a short int from the first & 2nd bytes"""
+			x = pydict[i] + pydict[i + 1]
+			m[row][col] = x
+			i +=  1*2
+def convert_list_to_bin():
+	fmt = '>H'
+	f = open('tmp_level.bin','wb')
+	for row in range(0, len(m)):
+		for col in range(0, len(m[0])):
+			f.write(pack(fmt,m[row][col]))
+	f.close()
 
 im = dwt.Image.open("../lena_512.png")
 pix = im.load()
 m = list(im.getdata())
+#print len(m)
 m = [m[i:i+im.size[0]] for i in range(0, len(m), im.size[0])]
-# Cast every item in the list to a float:
-#for row in range(0, len(m)):
-#        for col in range(0, len(m[0])):
-#                m[row][col] = float(m[row][col])
-# Perform a forward CDF 9/7 transform on the image:
+#print len(m[0]), len(m[1])
+"""Converts the 16bit to list m[row][col] this is the procedure that will be needed in the FPGA"""
+convert_intelhex_to_list()
 m = dwt.fwt97_2d(m, 1)
-# Convert the list of lists matrix to an image.    
+# Convert the list of lists matrix to an image.
 dwt.seq_to_img(m, pix)
+convert_list_to_bin()
 # Save the transformed image.
 im.save("test1_512_fwt.png")
 w, h = im.size
