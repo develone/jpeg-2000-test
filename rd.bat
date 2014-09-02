84a68ef054af92201a92ec8921a0c7fe12cc9f48
Edward Vidal <vidal@ws009.swbell.net>
2014-09-02 14:32:34 -0600

    chg rd.bat from 1ff 1fff

--- rd.bat
+++ rd.bat
@@ -1,7 +1,7 @@
 m:
 dir ttt.hex
 del ttt.hex
-"c:\Program Files (x86)\XSTOOLs\xsload.exe" -usb 0 -f hex -u 0x0000 0x1FF -ram ttt.hex
+"c:\Program Files (x86)\XSTOOLs\xsload.exe" -usb 0 -f hex -u 0x0000 0x1FFF -ram ttt.hex
 dir ttt.hex
 copy ttt.hex "c:\Users\vidal\Documents\GitHub\jpeg-2000-test\ipython_fixbv"
 dir  "c:\Users\vidal\Documents\GitHub\jpeg-2000-test\ipython_fixbv\ttt.hex"
