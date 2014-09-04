ae9cfe9741ca5f03f95a1592bfd78304b3d40612
Edward Vidal Jr <develone@sbcglobal.net>
2014-09-04 14:03:33 -0600

    cleaning up extra files

--- SdramSPInst/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "secureip" -o "C:/Xilinx/14.7/ISE_DS/SdramSPInst/SdramSPIinstTB_isim_beh.exe" -prj "C:/Xilinx/14.7/ISE_DS/SdramSPInst/SdramSPIinstTB_beh.prj" "work.SdramSPIinstTB" 
