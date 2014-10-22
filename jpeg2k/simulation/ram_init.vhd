f4e8316919f009467656d5224b40287f784a97fb
develone <develone@sbcglobal.net>
2014-10-22 08:00:45 -0700

    modified to 16 bit using rams_10c.data

--- jpeg2k/simulation/ram_init.vhd
+++ jpeg2k/simulation/ram_init.vhd
@@ -9,12 +9,12 @@ entity rams_20c is
 port(clk : in std_logic;
     we : in std_logic;
     addr : in std_logic_vector(5 downto 0);
-    din : in std_logic_vector(31 downto 0);
-    dout : out std_logic_vector(31 downto 0));
+    din : in std_logic_vector(15 downto 0);
+    dout : out std_logic_vector(15 downto 0));
 end rams_20c;
 architecture syn of rams_20c is
 
-    type RamType is array(0 to 63) of bit_vector(31 downto 0);
+    type RamType is array(0 to 2) of bit_vector(15 downto 0);
 impure function InitRamFromFile (RamFileName : in string) return RamType is
     FILE RamFile : text is in RamFileName;
     variable RamFileLine : line;
@@ -34,6 +34,7 @@ begin
             if clk'event and clk = '1' then
         if we = '1' then
         RAM(conv_integer(addr)) <= to_bitvector(din);
+		  --writeline(output, RAM);
         end if;
         dout <= to_stdlogicvector(RAM(conv_integer(addr)));
     end if;
