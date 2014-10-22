6dcdba160c97a067f70b9c8d2defb770cf008762
develone <develone@sbcglobal.net>
2014-10-22 12:36:17 -0700

    write_data.py writes to std_out the values of the lena64 image 10100000 with vi convert 0000000010100000 these will be the left sam and right inputs

--- jpeg2k/simulation/ram_init.vhd
+++ jpeg2k/simulation/ram_init.vhd
@@ -14,29 +14,32 @@ port(clk : in std_logic;
 end rams_20c;
 architecture syn of rams_20c is
 
-    type RamType is array(0 to 2) of bit_vector(15 downto 0);
+    type RamType is array(0 to 31) of bit_vector(15 downto 0);
 impure function InitRamFromFile (RamFileName : in string) return RamType is
     FILE RamFile : text is in RamFileName;
     variable RamFileLine : line;
-    variable RAM : RamType;
+    variable leftRAM : RamType;
+	 variable rightRAM : RamType;
 
 begin
     for I in RamType'range loop
         readline (RamFile, RamFileLine);
-        read (RamFileLine, RAM(I));
+        read (RamFileLine, leftRAM(I));
         end loop;
-    return RAM;
+    return leftRAM;
     end function;
-    signal RAM : RamType := InitRamFromFile("rams_20c.data");
+    signal leftRAM : RamType := InitRamFromFile("left.data");
     begin
     process (clk)
         begin
             if clk'event and clk = '1' then
         if we = '1' then
-        RAM(conv_integer(addr)) <= to_bitvector(din);
-		  --writeline(output, RAM);
+        leftRAM(conv_integer(addr)) <= to_bitvector(din);
+ 
         end if;
-        dout <= to_stdlogicvector(RAM(conv_integer(addr)));
+        dout <= to_stdlogicvector(leftRAM(conv_integer(addr)));
     end if;
 end process;
+
+ 
 end syn;
