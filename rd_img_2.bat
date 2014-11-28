0ad6eedae5f0d632b6fb1815781bfcfaa1728c79
develone <develone@sbcglobal.net>
2014-11-28 08:26:10 -0800

    latest bit file

--- rd_img_2.bat
+++ rd_img_2.bat
@@ -1,7 +1,7 @@
 m:
 dir ttt.hex
 del ttt.hex
-"c:\Program Files (x86)\XSTOOLs\xsload.exe" -usb 0 -f hex -u 0x80000 0xFFFFF -ram ttt.hex
+"c:\Program Files (x86)\XSTOOLs\xsload.exe" -usb 0 -f hex -u 0x0000 0xFFFFF -ram ttt.hex
 dir ttt.hex
 copy ttt.hex "c:\Users\vidal\Documents\GitHub\jpeg-2000-test\ipython_fixbv"
  
