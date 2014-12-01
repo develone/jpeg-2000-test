c6d92c29a67172a570a7f451c678778c3d5e1d0e
develone <develone@sbcglobal.net>
2014-12-01 05:14:17 -0800

    testing 256 x 256

--- ipython_fixbv/t_wave_53.py
+++ ipython_fixbv/t_wave_53.py
@@ -6,7 +6,7 @@ def convert_intelhex_to_list():
 	this is the procedure that will be needed in the FPGA"""
 
 	ih_lena = IntelHex()
-	ih_lena.loadhex('lena.hex')
+	ih_lena.loadhex('lena256.hex')
 	pydict = ih_lena.todict()
  	i = 0
 	x = 0
@@ -27,7 +27,7 @@ def convert_list_to_bin():
 			f.write(pack(fmt,m[row][col]))
 	f.close()
 
-im = dwt.Image.open("../lena_512.png")
+im = dwt.Image.open("../lena_256.png")
 pix = im.load()
 m = list(im.getdata())
 #print m.__sizeof__()
@@ -42,10 +42,9 @@ m = dwt.fwt97_2d(m, 1)
 dwt.seq_to_img(m, pix)
 convert_list_to_bin()
 # Save the transformed image.
-im.save("test1_512_fwt.png")
+im.save("test1_256_fwt.png")
 w, h = im.size
 m = dwt.upper_lower(m, w, h)
 mm = dwt.iwt97_2d(m, 1)
 dwt.seq_to_img(mm, pix)
-im.save("test1_512_iwt.png")
-
+im.save("test1_256_iwt.png") 
