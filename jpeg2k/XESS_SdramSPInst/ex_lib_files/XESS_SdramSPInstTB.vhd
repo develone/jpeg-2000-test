c3336574fc78672d5592245a96d36df9775150ac
develone <develone@sbcglobal.net>
2014-12-31 16:29:13 -0800

    With lena 256 x 256
    0b00000001111111100000001 0x000f 0 0x009c 0x00a4 0x00a4 0x7
    0x0000000000000
    It appears that these values are being read from the sdram
    :10000000009C 009C 00A400A400A400A4009C00A4E8<<<<<<<<<<<<<<<<<<<<<<<
    
    :1002000000A4 00A4 00A400A400A400A4009C00A4D62<<<<<<<<<<<<<<<<<<<<<<<
    
    :1004000000A4 00A4 00A400A400A400A4009C009CDC2<<<<<<<<<<<<<<<<<<<<<<<

--- jpeg2k/XESS_SdramSPInst/ex_lib_files/XESS_SdramSPInstTB.vhd
+++ jpeg2k/XESS_SdramSPInst/ex_lib_files/XESS_SdramSPInstTB.vhd
@@ -115,7 +115,8 @@ ARCHITECTURE behavior OF XESS_SdramSPInstTb IS
    constant fpgaClk_period : time := 83.3333 ns; -- 12 MHz XuLA clock.
 ----signal needed by XESS_SdramSPinst.vhd and xess_jpeg_top.vhd*************************** 
   signal clk_s                    : std_logic;  -- Internal clock.
-  signal sumDut_s                 : std_logic_vector(106 downto 0);  -- Send sum back to PC.
+  signal sumDut_s                 : std_logic_vector(158 downto 0);  -- Send sum back to PC.
+  alias fromsiginDut_s is sumDut_s(158 downto 107);
   alias fromjpflgsDut_s is sumDut_s(106 downto 103);
   alias fromjprhDut_s is sumDut_s(102 downto 87);
   alias fromjpsaDut_s is sumDut_s(86 downto 71);
@@ -128,13 +129,12 @@ ARCHITECTURE behavior OF XESS_SdramSPInstTb IS
   signal dataFromSdram_s          : std_logic_vector(sdData_io'range);  -- Data.
   signal addrSdram_s              : unsigned(22 downto 0);  -- Address.
   signal dataToSdram_s            : unsigned(15 downto 0);  -- Data.
-  signal dataFromRam_r, dataFromRam_r1, dataFromRam_r2  : unsigned(15 downto 0); 
+  signal dataFromRam_r  : unsigned(15 downto 0); 
   signal sum_r, sum_x             : unsigned( 15 downto 0);
   signal wr_s                     : std_logic;  -- Write-enable control.
   signal rd_s                     : std_logic;  -- Read-enable control.
   signal done_s                   : std_logic;  -- SDRAM R/W operation done signal.
   signal addr_r, addr_x           : unsigned(22 downto 0);  -- RAM address.
-  signal addr_r1, addr_r2           : unsigned(22 downto 0);  -- RAM address.
   signal dataToRam_r, dataToRam_x, dataFromRam_s : unsigned(15 downto 0);  -- Data to write to RAM.
 ----signal needed by XESS_SdramSPinst.vhd and xess_jpeg_top.vhd***************************
 --
@@ -144,15 +144,18 @@ ARCHITECTURE behavior OF XESS_SdramSPInstTb IS
   signal noupdate_s : std_logic;
   signal res_s : signed(15 downto 0) := (others => '0');
   signal res_u : unsigned(15 downto 0) := (others => '0');
-  signal jp_lf : unsigned(15 downto 0) := (others => '0');
-  signal jp_sa: unsigned(15 downto 0) := (others => '0');
-  signal jp_rh : unsigned(15 downto 0) := (others => '0');
-  signal jp_flgs : unsigned(3 downto 0) := (others => '0');
-  signal reset_col : std_logic := '0';
-  signal rdy : std_logic := '1';
-  signal addr_not_reached : std_logic := '0';
+  signal jp_lf_r : unsigned(15 downto 0) := (others => '0');
+  signal jp_sa_r: unsigned(15 downto 0) := (others => '0');
+  signal jp_rh_r : unsigned(15 downto 0) := (others => '0');
+  signal jp_lf_x : unsigned(15 downto 0) := (others => '0');
+  signal jp_sa_x: unsigned(15 downto 0) := (others => '0');
+  signal jp_rh_x : unsigned(15 downto 0) := (others => '0');
+
+  signal jp_flgs_r, jp_flgs_x : unsigned(3 downto 0) := (others => '0');
+  signal reset_col_r, reset_col_x : std_logic := '0';
+  signal rdy_r, rdy_x : std_logic := '1';
+  signal addr_not_reached_r, addr_not_reached_x : std_logic := '0';
   signal offset_r, offset_x           : unsigned(22 downto 0);  -- RAM address.
-  signal muxsel_r, muxsel_x  : std_logic :=  '0';
   signal col_r, col_x, row_r, row_x : unsigned(7 downto 0) := (others => '0');
 ----signal needed by xess_jpeg_top.vhd*************************** 
 
@@ -177,29 +180,33 @@ ARCHITECTURE behavior OF XESS_SdramSPInstTb IS
 component xess_jpeg_top is
     port (
         clk_fast: in std_logic;
-        addr_r: out unsigned(22 downto 0);
+        addr_r: inout unsigned(22 downto 0);
         addr_x: inout unsigned(22 downto 0);
         state_r: inout t_enum_t_State_1;
         state_x: inout t_enum_t_State_1;
-        addr_r1: inout unsigned(22 downto 0);
-        addr_r2: inout unsigned(22 downto 0);
         dataToRam_r: inout unsigned(15 downto 0);
         dataToRam_x: inout unsigned(15 downto 0);
-        dataFromRam_r: out unsigned(15 downto 0);
-        dataFromRam_r1: inout unsigned(15 downto 0);
-        dataFromRam_r2: in unsigned(15 downto 0);
+        dataFromRam_r: inout unsigned(15 downto 0);
         dataFromRam_x: inout unsigned(15 downto 0);
         sig_in: inout unsigned(51 downto 0);
         noupdate_s: out std_logic;
         res_s: inout signed (15 downto 0);
 		  res_u: out unsigned(15 downto 0);
-        jp_lf: inout unsigned(15 downto 0);
-        jp_sa: inout unsigned(15 downto 0);
-        jp_rh: inout unsigned(15 downto 0);
-        jp_flgs: inout unsigned(3 downto 0);
-        reset_col: inout std_logic;
-        rdy: in std_logic;
-        addr_not_reached: inout std_logic;
+        jp_lf_r: inout unsigned(15 downto 0);
+        jp_lf_x: inout unsigned(15 downto 0);
+        jp_sa_r: inout unsigned(15 downto 0);
+        jp_sa_x: inout unsigned(15 downto 0);
+        jp_rh_r: inout unsigned(15 downto 0);
+        jp_rh_x: inout unsigned(15 downto 0);
+
+        jp_flgs_r: inout unsigned(3 downto 0);
+		  jp_flgs_x: inout unsigned(3 downto 0);
+        reset_col_r: inout std_logic;
+		  reset_col_x: inout std_logic;
+        rdy_r: inout std_logic;
+		  rdy_x: inout std_logic;
+        addr_not_reached_r: inout std_logic;
+		  addr_not_reached_x: inout std_logic;
 		  offset_r: inout unsigned(22 downto 0);
         offset_x: inout unsigned(22 downto 0);
         dataFromRam_s: in unsigned(15 downto 0);
@@ -208,8 +215,6 @@ component xess_jpeg_top is
         rd_s: out std_logic;
         sum_r: inout unsigned(15 downto 0);
         sum_x: inout unsigned(15 downto 0);
-        muxsel_r: inout std_logic;
-        muxsel_x: inout std_logic;
         empty_r: out std_logic;
         full_r: out std_logic;
         enr_r: inout std_logic;
@@ -226,6 +231,7 @@ component xess_jpeg_top is
         col_x: inout unsigned(7 downto 0);
 		  row_r: inout unsigned(7 downto 0);
         row_x: inout unsigned(7 downto 0)
+ 
     );
 end component xess_jpeg_top;
  
@@ -242,24 +248,27 @@ xess_jpeg_top_u0 : xess_jpeg_top
 	  addr_x => addr_x,
 	  state_r => state_r,
 	  state_x => state_x,
-	  addr_r1 => addr_r1,
-     addr_r2 => addr_r2,
 	  dataToRam_r => dataToRam_r,
 	  dataToRam_x => dataToRam_x,
 	  dataFromRam_r =>  dataFromRam_r,
-	  dataFromRam_r1 =>  dataFromRam_r1,
-	  dataFromRam_r2  => dataFromRam_r2,
 	  sig_in => sig_in,
 	  noupdate_s => noupdate_s,
 	  res_s => res_s,
 	  res_u => res_u,
-	  jp_lf => jp_lf,
-	  jp_sa => jp_sa,
-	  jp_rh => jp_rh,
-	  jp_flgs => jp_flgs,
-	  reset_col => reset_col,
-	  rdy => rdy,
-	  addr_not_reached => addr_not_reached,
+	  jp_lf_r => jp_lf_r,
+	  jp_sa_r => jp_sa_r,
+	  jp_rh_r => jp_rh_r,
+	  jp_lf_x => jp_lf_x,
+	  jp_sa_x => jp_sa_x,
+	  jp_rh_x => jp_rh_x,
+	  jp_flgs_r => jp_flgs_r,
+	  jp_flgs_x => jp_flgs_x,
+	  reset_col_r => reset_col_r,
+	  reset_col_x => reset_col_x,
+	  rdy_r => rdy_r,
+	  rdy_x => rdy_x,
+	  addr_not_reached_r => addr_not_reached_r,
+	  addr_not_reached_x => addr_not_reached_x,
      offset_r => offset_r,
 	  offset_x => offset_x,
      dataFromRam_s => dataFromRam_s,
@@ -268,9 +277,6 @@ xess_jpeg_top_u0 : xess_jpeg_top
 	  rd_s => rd_s,
 	  sum_r => sum_r,
 	  sum_x => sum_x,
- 
-	  muxsel_r => muxsel_r, 
-     muxsel_x => muxsel_x,
 	  empty_r => empty_r,
 	  full_r => full_r,
 	  enr_r => enr_r,
@@ -284,9 +290,9 @@ xess_jpeg_top_u0 : xess_jpeg_top
 	  dataout_x => dataout_x,
 	  datain_x => datain_x,
 	  col_x => col_x,
-	  col_r => col_r,
-	  row_x => row_x,
-	  row_r => row_r   
+	  col_r => col_r
+ 
+   
   ); 
 	-- Instantiate the Unit Under Test (UUT)
    uut: XESS_SdramSPInst PORT MAP (
@@ -369,11 +375,11 @@ xess_jpeg_top_u0 : xess_jpeg_top
   fromsdramaddrDut_s <= std_logic_vector(addr_r);
   fromsdramdataDut_s <= std_logic_vector(sum_r);
   fromresdataDut_s <= std_logic_vector(res_s);
-  fromjplfDut_s <= std_logic_vector(jp_lf);
-  fromjpsaDut_s <= std_logic_vector(jp_sa);
-  fromjprhDut_s <= std_logic_vector(jp_rh);
-  fromjpflgsDut_s <= std_logic_vector(jp_flgs);
-
+  fromjplfDut_s <= std_logic_vector(jp_lf_x);
+  fromjpsaDut_s <= std_logic_vector(jp_sa_x);
+  fromjprhDut_s <= std_logic_vector(jp_rh_x);
+  fromjpflgsDut_s <= std_logic_vector(jp_flgs_x);
+  fromsiginDut_s  <= std_logic_vector(sig_in);
    -- Stimulus process.
    -- This is not used in this testbench. The FSM in the
    -- UUT steps through all the operations.
