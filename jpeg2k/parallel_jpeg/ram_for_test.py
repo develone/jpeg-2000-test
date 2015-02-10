3c9d45ab323afbe2cc55256f0a562919368f0324
develone <develone@sbcglobal.net>
2015-02-09 19:08:20 -0800

    the 4 ram instances are for testing     instance_ram_lf = ram(dout_lf, din_lf, addr_lf, we_lf, clk)     instance_ram_sa = ram(dout_sa, din_sa, addr_sa, we_sa, clk)     instance_ram_rh = ram(dout_rh, din_rh, addr_rh, we_rh, clk)     instance_ram_flgs = ram(dout_flgs, din_flgs, addr_flgs, we_flgs, clk)

---
+++ jpeg2k/parallel_jpeg/ram_for_test.py
@@ -0,0 +1,48 @@
+from myhdl import *
+
+def ram(dout, din, addr, we, clk, depth=2):
+    """  Ram model """
+    
+    mem = [Signal(intbv(0)[8:]) for i in range(depth)]
+    
+    @always(clk.posedge)
+    def write():
+        if we:
+            mem[addr].next = din
+                
+    @always_comb
+    def read():
+        dout.next = mem[addr]
+
+    return write, read
+
+
+dout_lf = Signal(intbv(0)[8:])
+din_lf = Signal(intbv(0)[8:])
+addr_lf = Signal(intbv(0)[7:])
+we_lf = Signal(bool(0))
+
+dout_sa = Signal(intbv(0)[8:])
+din_sa = Signal(intbv(0)[8:])
+addr_sa = Signal(intbv(0)[7:])
+we_sa = Signal(bool(0))
+
+dout_rh = Signal(intbv(0)[8:])
+din_rh = Signal(intbv(0)[8:])
+addr_rh = Signal(intbv(0)[7:])
+we_rh = Signal(bool(0))
+
+dout_res = Signal(intbv(0)[8:])
+din_res = Signal(intbv(0)[8:])
+addr_res = Signal(intbv(0)[7:])
+we_res = Signal(bool(0))
+
+clk = Signal(bool(0))
+
+def xx():
+    toVerilog.name = 'ram_1'
+    toVerilog(ram, dout, din, addr, we, clk)
+    toVHDL(ram, dout, din, addr, we, clk)
+    
+if __name__ == '__main__':
+    main()
