fe789f6ab1c8a8586dfb1108db38eaa3827bfba6
Edward Vidal Jr <develone@sbcglobal.net>
2015-02-22 18:39:41 -0700

    testing 16 operations

---
+++ jpeg2k/parallel_jpeg/merge_sam.py
@@ -0,0 +1,94 @@
+from myhdl import *
+from jpeg_constants import *
+def merge( left_com_x, sam_com_x, right_com_x, lft_s_i, sa_s_i, rht_s_i, merge_rdy_s, nomerge_s, W0=3, LVL0=4, W1=3, LVL1=4, W2=3, LVL2=4,  W3=3, LVL3=4, SIMUL=0 ):
+	lft_s = [lft_s_i((i+1)*W2, i*W2) for i in range(0, LVL2) ]
+	sa_s = [sa_s_i((i+1)*W2, i*W2) for i in range(0, LVL2) ]
+	rht_s = [rht_s_i((i+1)*W2, i*W2) for i in range(0, LVL2) ]
+	if(LVL0==2):
+		@always_comb
+		def merge_logic():
+			if (merge_rdy_s == 1):
+				left_com_x.next = concat(lft_s[1],lft_s[0])
+				sam_com_x.next = concat(sa_s[1],sa_s[0])
+				right_com_x.next = concat(rht_s[1],rht_s[0])
+				nomerge_s.next = 0
+			else:
+				left_com_x.next = 0
+				sam_com_x.next = 0
+				right_com_x.next = 0
+				nomerge_s.next = 1
+
+	elif(LVL0==4):
+		@always_comb
+		def merge_logic():
+			if (merge_rdy_s == 1):
+				left_com_x.next = concat(lft_s[3],lft_s[2],lft_s[1],lft_s[0])
+				sam_com_x.next = concat(sa_s[3],sa_s[2],sa_s[1],sa_s[0])
+				right_com_x.next = concat(rht_s[3],rht_s[2],rht_s[1],rht_s[0])
+				nomerge_s.next = 0
+			else:
+				left_com_x.next = 0
+				sam_com_x.next = 0
+				right_com_x.next = 0
+				nomerge_s.next = 1
+
+	elif(LVL0==8):
+		@always_comb
+		def merge_logic():
+			if (merge_rdy_s == 1):
+				left_com_x.next = concat(lft_s[7],lft_s[6],lft_s[5],lft_s[4],
+				lft_s[3],lft_s[2],lft_s[1],lft_s[0])
+				sam_com_x.next = concat(sa_s[7],sa_s[6],sa_s[5],sa_s[4],
+				sa_s[3],sa_s[2],sa_s[1],sa_s[0])
+				right_com_x.next = concat(rht_s[7],rht_s[6],rht_s[5],rht_s[4],
+				rht_s[3],rht_s[2],rht_s[1],rht_s[0])
+				nomerge_s.next = 0
+			else:
+				left_com_x.next = 0
+				sam_com_x.next = 0
+				right_com_x.next = 0
+				nomerge_s.next = 1
+	elif(LVL0==16):
+		@always_comb
+		def merge_logic():
+			if (merge_rdy_s == 1):
+				left_com_x.next = concat(lft_s[15],lft_s[14],lft_s[13],lft_s[12],
+				lft_s[11],lft_s[10],lft_s[9],lft_s[8],
+				lft_s[7],lft_s[6],lft_s[5],lft_s[4],
+				lft_s[3],lft_s[2],lft_s[1],lft_s[0])
+				sam_com_x.next = concat(sa_s[15],sa_s[14],sa_s[13],sa_s[12],
+				sa_s[11],sa_s[10],sa_s[9],sa_s[8],
+				sa_s[7],sa_s[6],sa_s[5],sa_s[4],
+				sa_s[3],sa_s[2],sa_s[1],sa_s[0])
+				right_com_x.next = concat(rht_s[15],rht_s[14],rht_s[13],rht_s[12],
+				rht_s[11],rht_s[10],rht_s[9],rht_s[8],
+				rht_s[7],rht_s[6],rht_s[5],rht_s[4],
+				rht_s[3],rht_s[2],rht_s[1],rht_s[0])
+				nomerge_s.next = 0
+			else:
+				left_com_x.next = 0
+				sam_com_x.next = 0
+				right_com_x.next = 0
+				nomerge_s.next = 1
+	return merge_logic
+def convert():
+	if (SIMUL == 0):
+		left_com_x = Signal(intbv(0)[LVL2*W2:])
+		sam_com_x = Signal(intbv(0)[LVL2*W2:])
+		right_com_x = Signal(intbv(0)[LVL2*W2:])
+		""" W0, LVL0, W1, LVL1, W2, LVL2, W3, and LVL3
+		Required  by jp_process
+		these are used to set the size of the
+		arrays"""
+		merge_rdy_s = Signal(bool(0))
+		nomerge_s = Signal(bool(0))
+		lft_s_i = Signal(intbv(0)[LVL2*W2:])
+		sa_s_i = Signal(intbv(0)[LVL2*W2:])
+		rht_s_i = Signal(intbv(0)[LVL2*W2:])
+		dut = toVHDL(merge, left_com_x, sam_com_x, right_com_x, lft_s_i, sa_s_i, rht_s_i, merge_rdy_s, nomerge_s, W0=W0, LVL0=LVL0, W1=W1, LVL1=LVL1, W2=W2, LVL2=LVL2,  W3=W3, LVL3=LVL3, SIMUL=0 )
+		dut = toVerilog(merge, left_com_x, sam_com_x, right_com_x, lft_s_i, sa_s_i, rht_s_i, merge_rdy_s, nomerge_s, W0=W0, LVL0=LVL0, W1=W1, LVL1=LVL1, W2=W2, LVL2=LVL2,  W3=W3, LVL3=LVL3, SIMUL=0 )
+	else:
+		print "In simulation mode"
+
+
+convert()
--- jpeg2k/parallel_jpeg/test_bench_array_jpeg.py
+++ jpeg2k/parallel_jpeg/test_bench_array_jpeg.py
@@ -1,21 +1,21 @@
 from myhdl import *
-from ram_for_test import *
-#from jpeg_constants import *
+from rom import *
+
 CONTENT = [
-163, 160, 155,
-155, 157, 170]
-W0 = 8
-LVL0 = 2
-W1 = 8
-LVL1 = 2
-W2 = 8
-LVL2 = 2
+668288, 672388, 668288]
+
+W0 = 10
+LVL0 = 16
+W1 = W0
+LVL1 = 16
+W2 = 10
+LVL2 = 16
 W3 = 5
-LVL3 = 2
+LVL3 = 16
 SIMUL = 1
 from array_jpeg import jp_process
 clk_fast = Signal(bool(0))
-res_out_x = Signal(intbv(0, min= -128 ,max= 128))
+res_out_x = Signal(intbv(0, min= -2048 ,max= 2048))
 update_s = Signal(bool(0))
 noupdate_s = Signal(bool(0))
 
@@ -23,58 +23,84 @@ left_s_i = Signal(intbv(0)[LVL2*W2:])
 sam_s_i = Signal(intbv(0)[LVL2*W2:])
 right_s_i = Signal(intbv(0)[LVL2*W2:])
 flgs_s_i = Signal(intbv(0)[LVL3*W3:])
-#dut = toVerilog(jp_process, res_out_x, left_s_i,sam_s_i, right_s_i, flgs_s_i, noupdate_s, update_s,  W0=W0, LVL0=LVL0, W1=W1, LVL1=LVL1, W2=W2, LVL2=LVL2, W3=W3, LVL3=LVL3)
-#dut = jp_process( res_out_x, left_s_i,sam_s_i, right_s_i, flgs_s_i, noupdate_s, update_s,  W0=W0, LVL0=LVL0, W1=W1, LVL1=LVL1, W2=W2, LVL2=LVL2, W3=W3, LVL3=LVL3)
-def tb(clk_fast, res_out_x, left_s_i,sam_s_i, right_s_i, flgs_s_i, noupdate_s, update_s,  W0=W0, LVL0=LVL0, W1=W1, LVL1=LVL1, W2=W2, LVL2=LVL2, W3=W3, LVL3=LVL3):
-    instance_ram_lf = ram(dout_lf, din_lf, addr_lf, we_lf, clk_fast)
-    instance_ram_sa = ram(dout_sa, din_sa, addr_sa, we_sa, clk_fast)
-    instance_ram_rh = ram(dout_rh, din_rh, addr_rh, we_rh, clk_fast)
-    instance_ram_flgs = ram(dout_flgs, din_flgs, addr_flgs, we_flgs, clk_fast)
-    instance_dut = jp_process( res_out_x, left_s_i,sam_s_i, right_s_i, flgs_s_i, noupdate_s, update_s,  W0=W0, LVL0=LVL0, W1=W1, LVL1=LVL1, W2=W2, LVL2=LVL2, W3=W3, LVL3=LVL3, SIMUL=SIMUL)
-    @always(delay(10))
-    def clkgen():
-        clk_fast.next = not clk_fast
-    
-    @instance
-    def stimulus():
-        for i in range(10):
-            print("%3d  ") % (now())
-            yield clk_fast.posedge
-            we_lf.next = 1
-            yield clk_fast.posedge
-            we_sa.next = 1
-            yield clk_fast.posedge
-            we_rh.next = 1
-            yield clk_fast.posedge
-            addr_lf.next = 0
-            addr_sa.next = 0
-            addr_rh.next = 0
-            yield clk_fast.posedge
-            for i in range(2):
-                din_lf.next = CONTENT[i]
-                din_sa.next = CONTENT[i + 1]
-                din_rh.next = CONTENT[i + 2]
-                addr_lf.next = addr_lf + 1
-                addr_sa.next = addr_lf + 1
-                addr_rh.next = addr_lf + 1
-                yield clk_fast.posedge
-            we_lf.next = 0
-            we_sa.next = 0
-            we_rh.next = 0
-            yield clk_fast.posedge
-            for i in range(2):
-                left_s_i.next = dout_lf << 8
-        raise StopSimulation
-    
-    
-    return instances()
-    
-tb(clk_fast, res_out_x, left_s_i,sam_s_i, right_s_i, flgs_s_i, noupdate_s, update_s,  W0=W0, LVL0=LVL0, W1=W1, LVL1=LVL1, W2=W2, LVL2=LVL2, W3=W3, LVL3=LVL3)
-#print dir(tb)
-#print tb
+from merge_sam import merge
+left_com_x = Signal(intbv(0)[LVL2*W2:])
+sam_com_x = Signal(intbv(0)[LVL2*W2:])
+right_com_x = Signal(intbv(0)[LVL2*W2:])
+lft_s_i = Signal(intbv(0)[LVL2*W2:])
+sa_s_i = Signal(intbv(0)[LVL2*W2:])
+rht_s_i = Signal(intbv(0)[LVL2*W2:])
+merge_rdy_s = Signal(bool(0))
+nomerge_s = Signal(bool(0))
 
-tb_fsm = traceSignals(tb, clk_fast, res_out_x, left_s_i,sam_s_i, right_s_i, flgs_s_i, noupdate_s, update_s)
-#print tb_fsm
-sim = Simulation(tb(clk_fast, res_out_x, left_s_i,sam_s_i, right_s_i, flgs_s_i, noupdate_s, update_s))
+
+def tb(clk_fast, res_out_x, left_s_i,sam_s_i, right_s_i, flgs_s_i,
+noupdate_s, update_s, left_com_x, sam_com_x, right_com_x,
+lft_s_i, sa_s_i, rht_s_i,
+merge_rdy_s, nomerge_s,
+W0=W0, LVL0=LVL0, W1=W1, LVL1=LVL1, W2=W2,
+LVL2=LVL2, W3=W3, LVL3=LVL3):
+
+	instance_rom_flgs = rom_flgs(dout_flgs, addr_flgs, ROM_CONTENT)
+	instance_merge = merge( left_com_x, sam_com_x, right_com_x,
+lft_s_i, sa_s_i, rht_s_i, merge_rdy_s, nomerge_s,
+W0=W0, LVL0=LVL0, W1=W1, LVL1=LVL1, W2=W2,
+LVL2=LVL2, W3=W3, LVL3=LVL3)
+	instance_dut = jp_process( res_out_x, left_s_i,sam_s_i, right_s_i,
+flgs_s_i, noupdate_s, update_s,  W0=W0, LVL0=LVL0, W1=W1, LVL1=LVL1,
+W2=W2, LVL2=LVL2, W3=W3, LVL3=LVL3, SIMUL=SIMUL)
+	@always(delay(10))
+	def clkgen():
+		clk_fast.next = not clk_fast
+
+	@instance
+	def stimulus():
+		for i in range(10):
+			print("%3d  ") % (now())
+
+			yield clk_fast.posedge
+		for i in range(2):
+
+			lft_s_i.next = (157<< 150) + (160 << 140)+(163<<130) + (156 << 120) + (157<<110) + (160 << 100)+(163<<90)+(156 << 80) +(160 << 70) + (163<<60)+(156 << 50) + (157<<40) + (160 << 30)+(163<<20)+(156 << 10) + 157
+			sa_s_i.next = (157<< 150) + (160 << 140)+(163<<130) + (156 << 120) + (157<<110) + (160 << 100)+(163<<90)+(156 << 80) +(160 << 70) + (163<<60)+(156 << 50) + (157<<40) + (160 << 30)+(163<<20)+(156 << 10) + 157
+			rht_s_i.next = (157<< 150) + (160 << 140)+(163<<130) + (156 << 120) + (157<<110) + (160 << 100)+(163<<90)+(156 << 80) +( 160 << 70) + (163<<60)+(156 << 50) + (157<<40) + (160 << 30)+(163<<20)+(156 << 10) + 157
+
+ 			yield clk_fast.posedge
+			merge_rdy_s.next = 1
+			yield clk_fast.posedge
+
+ 		for i in range(2):
+
+			left_s_i.next = left_com_x
+			sam_s_i.next = sam_com_x
+			right_s_i.next = right_com_x
+ 			yield clk_fast.posedge
+		addr_flgs.next = 0
+		for i in range(26):
+			flgs_s_i.next = dout_flgs
+			yield clk_fast.posedge
+			addr_flgs.next = addr_flgs + 1
+			yield clk_fast.posedge
+			update_s.next = 1
+			yield clk_fast.posedge
+			update_s.next = 0
+			yield clk_fast.posedge
+			update_s.next = 1
+			yield clk_fast.posedge
+			update_s.next = 0
+			yield clk_fast.posedge
+		raise StopSimulation
+	return instances()
+tb(clk_fast, res_out_x, left_s_i,sam_s_i, right_s_i, flgs_s_i,
+noupdate_s, update_s, left_com_x, sam_com_x, right_com_x,
+lft_s_i, sa_s_i, rht_s_i,
+merge_rdy_s, nomerge_s,
+W0=W0, LVL0=LVL0, W1=W1, LVL1=LVL1, W2=W2,
+LVL2=LVL2, W3=W3, LVL3=LVL3)
+tb_fsm = traceSignals(
+tb, clk_fast, res_out_x, left_s_i,sam_s_i, right_s_i, flgs_s_i,
+noupdate_s, update_s, left_com_x, sam_com_x, right_com_x,
+lft_s_i, sa_s_i, rht_s_i,
+merge_rdy_s, nomerge_s)
 sim = Simulation(tb_fsm)
 sim.run()
