3624ed59aa339deb0a42662326ee0310fc8fc0ac
Edward Vidal Jr <develone@sbcglobal.net>
2015-02-23 18:19:02 -0700

    continued test of 16 results showing how the right becomes the left of next

--- jpeg2k/parallel_jpeg/rom.py
+++ jpeg2k/parallel_jpeg/rom.py
@@ -1,10 +1,12 @@
 from myhdl import *
-ROM_CONTENT = ( 7, 5, 6, 4,
- 224, 160, 192, 128,
- 7168, 5120, 6144,4096,
-229376, 163840,196608, 131072,
-7, 0, 7, 0, 7, 0,
-6, 0, 6, 0, 6, 0)
+ROM_CONTENT = ( 7, 7<<5, 7<<10, 7<<15,
+ 7<<20, 7<<25, 7<<30, 7<<35,
+ 7<<40, 7<<45, 7<<50, 7<<55,
+7<<60, 7<<65,7<<70, 7<<75,
+5, 5<<5, 5<<10, 5<<15,
+ 5<<20, 5<<25, 5<<30, 5<<35,
+ 5<<40, 5<<45, 5<<50, 5<<55,
+5<<60, 5<<65,5<<50, 5<<75)
 RADZ = 10
 
 def rom_flgs(dout_flgs, addr_flgs, ROM_CONTENT):
@@ -21,7 +23,7 @@ def romconvert():
     toVHDL(rom_flgs, dout_flgs, addr_flgs, ROM_CONTENT)
 
 
-dout_flgs = Signal(intbv(0)[20:])
+dout_flgs = Signal(intbv(0)[80:])
 addr_flgs = Signal(intbv(0)[RADZ::])
 
 
--- jpeg2k/parallel_jpeg/test_bench_array_jpeg.py
+++ jpeg2k/parallel_jpeg/test_bench_array_jpeg.py
@@ -1,21 +1,22 @@
 from myhdl import *
 from rom import *
-
+from array_jpeg import jp_process
+from merge_sam import merge
 CONTENT = [
 668288, 672388, 668288]
 
-W0 = 10
+W0 = 8
 LVL0 = 16
-W1 = W0
+W1 = 8
 LVL1 = 16
-W2 = 10
+W2 = 8
 LVL2 = 16
 W3 = 5
 LVL3 = 16
 SIMUL = 1
-from array_jpeg import jp_process
+
 clk_fast = Signal(bool(0))
-res_out_x = Signal(intbv(0, min= -2048 ,max= 2048))
+res_out_x = Signal(intbv(0, min= -(2**(W0+1)) ,max= (2**(W0+1))))
 update_s = Signal(bool(0))
 noupdate_s = Signal(bool(0))
 
@@ -23,7 +24,7 @@ left_s_i = Signal(intbv(0)[LVL2*W2:])
 sam_s_i = Signal(intbv(0)[LVL2*W2:])
 right_s_i = Signal(intbv(0)[LVL2*W2:])
 flgs_s_i = Signal(intbv(0)[LVL3*W3:])
-from merge_sam import merge
+
 left_com_x = Signal(intbv(0)[LVL2*W2:])
 sam_com_x = Signal(intbv(0)[LVL2*W2:])
 right_com_x = Signal(intbv(0)[LVL2*W2:])
@@ -56,33 +57,36 @@ W2=W2, LVL2=LVL2, W3=W3, LVL3=LVL3, SIMUL=SIMUL)
 	@instance
 	def stimulus():
 		for i in range(10):
-			print("%3d  ") % (now())
+			print( "%3d ") % (now())
 
 			yield clk_fast.posedge
 		for i in range(2):
-
-			lft_s_i.next = (157<< 150) + (160 << 140)+(163<<130) + (156 << 120) + (157<<110) + (160 << 100)+(163<<90)+(156 << 80) +(160 << 70) + (163<<60)+(156 << 50) + (157<<40) + (160 << 30)+(163<<20)+(156 << 10) + 157
-			sa_s_i.next = (157<< 150) + (160 << 140)+(163<<130) + (156 << 120) + (157<<110) + (160 << 100)+(163<<90)+(156 << 80) +(160 << 70) + (163<<60)+(156 << 50) + (157<<40) + (160 << 30)+(163<<20)+(156 << 10) + 157
-			rht_s_i.next = (157<< 150) + (160 << 140)+(163<<130) + (156 << 120) + (157<<110) + (160 << 100)+(163<<90)+(156 << 80) +( 160 << 70) + (163<<60)+(156 << 50) + (157<<40) + (160 << 30)+(163<<20)+(156 << 10) + 157
+			#10	9	8	8	7	6	6	5	4	4	3	2	2	1	0
+			lft_s_i.next = (160 << W0*15) + (163 << W0*14)+(156 << W0*13) + (157 << W0*12) + (160 << W0*11)+(163 << W0*10) + (156 << W0*9)+(160 << W0*8) +(159 << W0*7) + (144 << W0*6) + (157 <<  W0*5) + (163 << W0*4) + (161 << W0*3) + (159 << W0*2) + (157 << W0) + 158
+			sa_s_i.next = (158 << W0*15) + (159 << W0*14)+(160 << W0*13) + (161 << W0*12) + (162 << W0*11)+(163 << W0*10) + (164 << W0*9)+(165 << W0*8) +(166 << W0*7) + (165 << W0*6) + (164 <<  W0*5) + (163 << W0*4) + (162 << W0*3) + (161 << W0*2) + (160 << W0) + 159
+			rht_s_i.next = (157 << W0*15) + (160 << W0*14)+(163 << W0*13) + (156 << W0*12) + (157 << W0*11)+(160 << W0*10) + (163 << W0*9)+(156 << W0*8) +(160 << W0*7) + (159 << W0*6) + (144 <<  W0*5) + (157 << W0*4) + (163 << W0*3) + (161 << W0*2) + (159 << W0) + 157
 
  			yield clk_fast.posedge
 			merge_rdy_s.next = 1
 			yield clk_fast.posedge
 
  		for i in range(2):
-
+			print( "%3d %s %s %s") % (now(), hex(left_s_i), hex(sam_s_i), hex(right_s_i))
 			left_s_i.next = left_com_x
 			sam_s_i.next = sam_com_x
 			right_s_i.next = right_com_x
  			yield clk_fast.posedge
 		addr_flgs.next = 0
 		for i in range(26):
+
 			flgs_s_i.next = dout_flgs
 			yield clk_fast.posedge
+			#print( "%3d %s") % (now(), hex(flgs_s_i))
 			addr_flgs.next = addr_flgs + 1
 			yield clk_fast.posedge
 			update_s.next = 1
 			yield clk_fast.posedge
+			print ("%d %d %s") % (now(), res_out_x.signed(), bin(flgs_s_i) )
 			update_s.next = 0
 			yield clk_fast.posedge
 			update_s.next = 1
