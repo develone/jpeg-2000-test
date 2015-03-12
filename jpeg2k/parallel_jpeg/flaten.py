fcd22bccd2526350d12eb045c11783a00b1af979
Edward Vidal Jr <develone@sbcglobal.net>
2015-03-12 17:41:42 -0600

    adding testing values on code provided by Chris Felton
    0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
    1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
    1000111100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
    1000111110000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
    1000111110000101000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
    1000111110000101101000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
    1000111110000101101000000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
    1000111110000101101000000000000000000000100011110000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
    1000111110000101101000000000000000000000100011111000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
    1000111110000101101000000000000000000000100011111000010100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
    1000111110000101101000000000000000000000100011111000010110100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000

--- jpeg2k/parallel_jpeg/flaten.py
+++ jpeg2k/parallel_jpeg/flaten.py
@@ -20,5 +20,36 @@ def test_flatten():
 		yield delay(1)
 		print(bin(flat, 160))
 		assert flat[160-1] == 1
+		matrix[0][1].next = 0xf
+		yield delay(1)
+		print(bin(flat, 160))
+		matrix[0][2].next = 0x8
+		yield delay(1)
+		print(bin(flat, 160))
+		matrix[0][3].next = 0x5
+		yield delay(1)
+		print(bin(flat, 160))
+		matrix[0][4].next = 0xa
+		yield delay(1)
+		print(bin(flat, 160))
+
+ 		matrix[2][0].next = 0x8
+		yield delay(1)
+		print(bin(flat, 160))
+		#assert flat[160-1] == 1
+		matrix[2][1].next = 0xf
+		yield delay(1)
+		print(bin(flat, 160))
+		matrix[2][2].next = 0x8
+		yield delay(1)
+		print(bin(flat, 160))
+		matrix[2][3].next = 0x5
+		yield delay(1)
+		print(bin(flat, 160))
+		matrix[2][4].next = 0xa
+		yield delay(1)
+		print(bin(flat, 160))
+
+
 	return tbdut, tbstim
 Simulation(test_flatten()).run()
