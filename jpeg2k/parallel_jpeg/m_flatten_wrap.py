c7354fa1691cf0ea6febf099822a515f66be6888
develone <develone@sbcglobal.net>
2015-03-25 10:43:19 -0700

    Now creates jpeg2k/parallel_jpeg/matrix_wrap.v & jpeg2k/parallel_jpeg/matrix_wrap.v

--- jpeg2k/parallel_jpeg/m_flatten_wrap.py
+++ jpeg2k/parallel_jpeg/m_flatten_wrap.py
@@ -3,12 +3,7 @@ from flaten import m_flatten
 from jpeg_constants import *
 matrix = [[Signal(intbv(0)[W0:]) for mcol in range(4)] for mrow in range(4)]
 flat = Signal(intbv(0)[LVL2*W2:])
-matrix_lf = [[Signal(intbv(0)[W0:]) for mcol in range(4)] for mrow in range(4)]
-matrix_sa = [[Signal(intbv(0)[W0:]) for mcol in range(4)] for mrow in range(4)]
-matrix_rt = [[Signal(intbv(0)[W0:]) for mcol in range(4)] for mrow in range(4)]
-flat_lf = Signal(intbv(0)[LVL2*W2:])
-flat_sa = Signal(intbv(0)[LVL2*W2:])
-flat_rt = Signal(intbv(0)[LVL2*W2:])
+ 
 x = Signal(intbv(0, min=-(2**(W0)), max=(2**(W0))))
 z = Signal(intbv(0)[W0:])
 mrow = Signal(intbv(0)[4:])
@@ -17,8 +12,11 @@ mcol = Signal(intbv(0)[4:])
 def matrix_wrap(matrix, flat, z, x, mrow, mcol,
 W0=W0, LVL0=LVL0, W1=W1, LVL1=LVL1, W2=W2,
 LVL2=LVL2, W3=W3, LVL3=LVL3):
-    instance_mat = m_flatten(matrix, flat)
-    return  instance_mat
+    mat = m_flatten(matrix, flat)
+    return  mat
 toVHDL(matrix_wrap, matrix, flat, z, x, mrow, mcol,
+W0=W0, LVL0=LVL0, W1=W1, LVL1=LVL1, W2=W2,
+LVL2=LVL2, W3=W3, LVL3=LVL3)
+toVerilog(matrix_wrap, matrix, flat, z, x, mrow, mcol,
 W0=W0, LVL0=LVL0, W1=W1, LVL1=LVL1, W2=W2,
 LVL2=LVL2, W3=W3, LVL3=LVL3)
