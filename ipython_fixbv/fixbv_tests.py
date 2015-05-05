330f4bc1cc73d1cd38ac7d4cc813f3c728d2c671
develone <develone@sbcglobal.net>
2015-05-05 16:30:34 -0600

    now the simulation demonstrates addition, subtraction, and muliplication with gtkwave

--- ipython_fixbv/fixbv_tests.py
+++ ipython_fixbv/fixbv_tests.py
@@ -1,91 +1,89 @@
 from myhdl import *
+import  random
 #from myhdl._fixbv import FixedPointFormat as fpf
-ww = (32,9)
-x_sig = Signal(intbv(0)[ww[0]:])
-y_sig = Signal(intbv(0)[ww[0]:])
-sum_sig = Signal(intbv(0)[ww[0]:])
+
+
+
+def disp_fix(x_i):
+        iW = x_i._W
+	print float(x_i), int(x_i), repr(x_i), hex(x_i), bin(x_i, iW[0])
+
+x = (fixbv(3.1415926535897932, min = -2**10, max=2**10, res=1e-6))
+#disp_fix(x)
+y = (fixbv(510.5, min = -2**10, max=2**10, res=1e-6))
+#disp_fix(y)
+iW = x._W
+
+ww = (x._nrbits,iW[1])
+ 
+x = fixbv(3.141592)[ww]
+y = fixbv(510.5)[ww] 
+  
+x_sig = Signal(intbv(0, min = -2**(x._nrbits-1), max = 2**(x._nrbits-1) ))
+y_sig = Signal(intbv(0, min = -2**(x._nrbits-1), max = 2**(x._nrbits-1) ))
+
+sum_sig = Signal(intbv(0)[ww[0]+1:])
+sub_sig = Signal(intbv(0, min = -2**(x._nrbits), max = 2**(x._nrbits) ))
+ 
+
 prod_sig = Signal(intbv(0)[2*ww[0]:])
 clk = Signal(bool(0))
 do_add = Signal(bool(0))
 do_mul = Signal(bool(0))
-wL = ww[0]
-wI = (wL - ww[1]) - 1
-wF = wL - wI
-print  wL, wI, wF
-x = fixbv(3.141592)[ww]
-#ww = (32,12)  32 19 13
-# 2109876543210 1234567890123456789
-# 0000000000011 0010010000111111011
-# siiiiiiiiiiii.fffffffffffffffffff
+do_sub = Signal(bool(0))
 
-#ww = (32,9) frac 32 22 10
-# 9876543210 1234567890123456789012
-# 0000000011 0010010000111111011000 
-# siiiiiiiii.ffffffffffffffffffffff
+done_add = Signal(bool(0))
+done_sub = Signal(bool(0))
+done_mul = Signal(bool(0)) 
+ 
+def fixbv_sub(clk, do_sub, x_sig, y_sig, sub_sig, done_sub):
+	@always(clk.posedge)
+	def sub_rtl():
+		if (do_sub == 1):
+                        done_sub.next = 0
+			sub_sig.next = x_sig - y_sig
+		else:
+			done_sub.next = 1
+			sub_sig.next = 0
+	return sub_rtl
 
-#ww = (32,8) frac 32 23 9
-# 876543210 12345678901234567890123
-# 000000011 00100100001111110110000 
-# siiiiiiii.fffffffffffffffffffffff
-#print bin(x, wL)
-y = fixbv(0.5)[ww]
-
-z = x * y
-z1 = fixbv(z)[ww]
-'''
-print float(x), int(x), repr(x)
-print float(y), int(y), repr(y)
-print float(z), int(z), repr(z)
-print float(z1), int(z1), repr(z1)
-print
-
-z = x + y
-z1 = fixbv(z)[ww]
-print float(x), int(x), repr(x)
-print float(y), int(y), repr(y)
-print float(z), int(z), repr(z)
-print float(z1), int(z1), repr(z1) 
-print
-
-z = x - y
-z1 = fixbv(z)[ww]
-print float(x), int(x), repr(x)
-print float(y), int(y), repr(y)
-print float(z), int(z), repr(z)
-print float(z1), int(z1), repr(z1) 
-print
-
-z = y - x
-z1 = fixbv(z)[ww]
-print float(x), int(x), repr(x)
-print float(y), int(y), repr(y)
-print float(z), int(z), repr(z)
-print float(z1), int(z1), repr(z1)
-''' 
-def fixbv_add(clk, do_add, x_sig, y_sig, sum_sig):
+def fixbv_add(clk, do_add, x_sig, y_sig, sum_sig, done_add):
 	@always(clk.posedge)
 	def add_rtl():
 		if (do_add == 1):
+                        done_add.next = 0
 			sum_sig.next = x_sig + y_sig
 		else:
+			done_add.next = 1
 			sum_sig.next = 0
 	return add_rtl
 
-def fixbv_mul(clk, do_prod, x_sig, y_sig, prod_sig):
+ 
+def fixbv_mul(clk, do_mul, x_sig, y_sig, prod_sig, done_mul):
 	@always(clk.posedge)
 	def add_rtl():
 		if (do_mul == 1):
+			done_mul.next = 0
 			prod_sig.next = x_sig * y_sig
 		else:
+			done_mul.next = 1
 			prod_sig.next = 0
 	return add_rtl
-toVHDL(fixbv_add, clk, do_add, x_sig, y_sig, sum_sig)
-toVerilog(fixbv_add, clk, do_add, x_sig, y_sig, sum_sig)
-toVHDL(fixbv_mul, clk, do_mul, x_sig, y_sig, prod_sig)
-toVerilog(fixbv_mul, clk, do_mul, x_sig, y_sig, prod_sig)
+toVHDL(fixbv_add, clk, do_add, x_sig, y_sig, sum_sig, done_add)
+toVerilog(fixbv_add, clk, do_add, x_sig, y_sig, sum_sig, done_add)
+
+toVHDL(fixbv_sub, clk, do_sub, x_sig, y_sig, sub_sig, done_sub)
+toVerilog(fixbv_sub, clk, do_sub, x_sig, y_sig, sub_sig, done_sub)
+
+toVHDL(fixbv_mul, clk, do_mul, x_sig, y_sig, prod_sig, done_mul)
+toVerilog(fixbv_mul, clk, do_mul, x_sig, y_sig, prod_sig, done_mul)
+
+
 def tb():
-        dut_fixbv_add = fixbv_add(clk, do_add, x_sig, y_sig, sum_sig)
-	dut_fixbv_mul = fixbv_mul(clk, do_mul, x_sig, y_sig, prod_sig)
+        dut_fixbv_add = fixbv_add(clk, do_add, x_sig, y_sig, sum_sig, done_add)
+	dut_fixbv_sub = fixbv_sub( clk, do_sub, x_sig, y_sig, sub_sig, done_sub)
+	dut_fixbv_mul = fixbv_mul(clk, do_mul, x_sig, y_sig, prod_sig, done_mul)
+ 
 	@always(delay(10))
     	def clkgen():
         	clk.next = not clk
@@ -95,32 +93,42 @@ def tb():
         	for i in range(10):
         	    print( "%3d ") % (now())
          	    yield clk.posedge
-		    x_sig.next = 13176792
+		    x_sig.next = int(x)
 		    yield clk.posedge
-		    y_sig.next = 2097152
+		    y_sig.next = int(y)
 		    yield clk.posedge
 		    do_add.next = 1
 		    yield clk.posedge 
 		    do_add.next = 0
 		    yield clk.posedge
+                    
+                    do_sub.next = 1
+		    yield clk.posedge 
+		    do_sub.next = 0
+		    yield clk.posedge
+
 		    do_mul.next = 1
 		    yield clk.posedge 
 		    do_mul.next = 0
-		    yield clk.posedge   
+		    yield clk.posedge
+                      
 		raise StopSimulation
-	return clkgen, stimulus, dut_fixbv_add, dut_fixbv_mul
+	return clkgen, stimulus, dut_fixbv_add, dut_fixbv_sub, dut_fixbv_mul
+print 'x + y'
 z = x + y
-z1 = fixbv(z)[ww]
-print float(x), int(x), repr(x), hex(x)
-print float(y), int(y), repr(y), hex(y)
-print float(z), int(z), repr(z), hex(z)
-
+disp_fix(x)
+disp_fix(y)
+disp_fix(z)
+#z1 = fixbv(z)[ww]
+z = x - y
+print 'x - y'
+disp_fix(z)
+ 
+print 'x * y'
 z = x * y
-z1 = fixbv(z)[ww]
-print float(x), int(x), repr(x), hex(x)
-print float(y), int(y), repr(y), hex(y)
-print float(z), int(z), repr(z), hex(z)
-print float(z1), int(z1), repr(z1), hex(z1)
+disp_fix(z)
+#z1 = fixbv(z)[ww]
+ 
 
 tb_fsm = traceSignals(tb)
 sim = Simulation(tb_fsm)
