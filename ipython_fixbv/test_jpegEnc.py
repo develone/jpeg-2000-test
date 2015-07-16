d273c7a2ac14882f350b4d7a17f19ba5088a2255
vidal <vidal@vidal-MX6438.(none)>
2015-07-16 06:31:13 -0600

    Now can use lena_128.png & lena_64.png as starting images. looking into how to encode final dwt results.

--- ipython_fixbv/test_jpegEnc.py
+++ ipython_fixbv/test_jpegEnc.py
@@ -1,12 +1,14 @@
  
 from _jpegEnc import JPEGEnc2k
-
+import math
  
 jp2k = JPEGEnc2k()
 #help(jp2k)
-jp2k.set_img_fn("../jpeg2k/parallel_jpeg/lena_rgb_512.png")
+#jp2k.set_img_fn("../jpeg2k/parallel_jpeg/lena_rgb_512.png")
 #jp2k.set_img_fn("../lena.jpg")
 #jp2k.set_img_fn("../lena_256.png")
+#jp2k.set_img_fn("../lena_128.png")
+jp2k.set_img_fn("../lena_64.png")
 #jp2k.set_img_fn("../grabber002.png")
 f = [5, 3]
 jp2k.set_filter(f)
@@ -22,6 +24,36 @@ if (jp2k.get_filter() == [9,7]):
 else:
     jp2k.fwd_dwt()
 im = jp2k.get_img()
+im = im.crop((0,0,32,32))
+print im.size
+n = list(im.getdata())
+
+print len(n)
+x = []
+p = []
+non_zero = 0
+for i in range(len(n)):
+    if (n[i] != 0):
+        non_zero = non_zero + 1
+        x.append(n[i])
+        #print n[i], math.log(n[i],2), -n[i]*math.log(n[i],2)
+        p.append(-n[i]*math.log(n[i],2))
+print x
+print min(x), max(x)
+print non_zero
+#print p
+#print sum(p)
+from collections import Counter
+print Counter(x)
+print 
+print Counter(x).most_common(32)
+print 
+print Counter(x).most_common(8)
+
+print 
+print Counter(x).values()
+
+print 
 
 im.save("test1_256_fwt.png")
  
