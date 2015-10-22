e0ab7bd6422e9b747a19c121548ee9c201b7b91f
develone <develone@sbcglobal.net>
2015-10-22 15:30:25 -0600

    removing x now using res_o as the input. tests now only positive values fails with negative values for i in range(0,2**(W0),1):

--- pc_fast_blinker_jpeg/signed2twoscomplement.py
+++ pc_fast_blinker_jpeg/signed2twoscomplement.py
@@ -3,42 +3,41 @@ import random
 '''unsigned data width signed is W0 + 1'''
 W0 = 8
 
-t = Signal(intbv(0, min= -(2**(W0)) ,max= (2**(W0))))
-x = Signal(intbv(0, min= -(2**(W0)) ,max= (2**(W0))))
+  
+res_o = Signal(intbv(0, min= -(2**(W0)) ,max= (2**(W0))))
 z = Signal(intbv(0)[W0:])
 clk = Signal(bool(0))
-def signed2twoscomplement(clk, x, z):
+def signed2twoscomplement(clk, res_o, z):
 	
 	@always(clk.posedge)
 	def unsigned_logic():
-		z.next = x
+		z.next = res_o
 
 	return unsigned_logic
 def convert():
-	toVHDL(signed2twoscomplement, clk, x, z)
-	toVerilog(signed2twoscomplement, clk, x, z)
-def tb(clk, x, z):
-    instance_1 = signed2twoscomplement(clk, x, z)
+	toVHDL(signed2twoscomplement, clk, res_o, z)
+	toVerilog(signed2twoscomplement, clk, res_o, z)
+def tb(clk, res_o, z):
+    instance_1 = signed2twoscomplement(clk, res_o, z)
     @always(delay(10))
     def clkgen():
         clk.next = not clk
     @instance
     def stimulus():
-        for i in range(-(2**(W0)),2**(W0),1):
-            t.next = random.randrange(-2**(W0-1),2**(W0-1))
-            #t.next = i
+        for i in range(0,2**(W0),1):
+             
+            res_o.next = i
             yield clk.posedge
-            x.next = t[W0:]
-            yield clk.posedge
-            print ("%d %d %d %d %s %s %s") % (t, x, z.signed(), z, bin(t,W0), bin(x,W0), bin(z, W0))
-            print ("%d %d %d %d %s %s %s") % (t, x, z.signed(), z, hex(t), hex(x), hex(z))
+            print ("%d %d %d %s %s ") % (res_o, z.signed(), z, bin(res_o,W0), bin(z, W0))
+            print ("%d %d %d %s %s ") % (res_o, z.signed(), z, hex(res_o), hex(z))
             if( z.signed() < 0):
 				print ("%d ") % (((2**W0)+z.signed()) + 1)
         raise StopSimulation
     return instances()
 '''
-tb_fsm = traceSignals(tb,clk, x, z)
+tb_fsm = traceSignals(tb,clk, res_o, z)
 sim = Simulation(tb_fsm)
 sim.run()
-'''
+
 convert()
+'''
