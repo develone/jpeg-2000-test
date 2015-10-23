35635b1f5775e7d3ef80a26fa2d411b1b8b9e3a3
develone <develone@sbcglobal.net>
2015-10-23 05:50:47 -0600

    signed2twoscomplement in python simulation requires steps t.next = i yield clk.posedge res_o.next = t[W0:] when negative values are used

--- pc_fast_blinker_jpeg/signed2twoscomplement.py
+++ pc_fast_blinker_jpeg/signed2twoscomplement.py
@@ -3,7 +3,7 @@ import random
 '''unsigned data width signed is W0 + 1'''
 W0 = 8
 
-  
+t = Signal(intbv(0, min= -(2**(W0)) ,max= (2**(W0))))  
 res_o = Signal(intbv(0, min= -(2**(W0)) ,max= (2**(W0))))
 z = Signal(intbv(0)[W0:])
 clk = Signal(bool(0))
@@ -24,14 +24,13 @@ def tb(clk, res_o, z):
         clk.next = not clk
     @instance
     def stimulus():
-        for i in range(0,2**(W0),1):
-             
-            res_o.next = i
+        for i in range(-2**(W0),2**(W0),1):
+            t.next = i
             yield clk.posedge
-            print ("%d %d %d %s %s ") % (res_o, z.signed(), z, bin(res_o,W0), bin(z, W0))
-            print ("%d %d %d %s %s ") % (res_o, z.signed(), z, hex(res_o), hex(z))
-            if( z.signed() < 0):
-				print ("%d ") % (((2**W0)+z.signed()) + 1)
+            res_o.next = t[W0:]
+            yield clk.posedge
+            print ("i = %4d 9 bits %s 8 bits %s ") % (i, bin(t,W0+1),bin(t,W0+1)) 
+            print ("res_o = %d z.signed() = %d z = %d 9 bits %s 8 bits %s ") % (res_o, z.signed(), z, bin(res_o,W0+1), bin(z, W0))
         raise StopSimulation
     return instances()
 '''
@@ -41,3 +40,4 @@ sim.run()
 
 convert()
 '''
+
