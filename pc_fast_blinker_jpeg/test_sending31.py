4654db196141ed21c347b6303c561a7906bffa3a
develone <develone@sbcglobal.net>
2016-01-21 12:27:29 -0700

    now both clkInOut & ld_o should be setting values

--- pc_fast_blinker_jpeg/test_sending31.py
+++ pc_fast_blinker_jpeg/test_sending31.py
@@ -2,15 +2,10 @@ from myhdl import *
 import argparse
 from div_clk import div_4
 from para2ser import para2ser
-#from jpeg_sig import *
-WIDTH_OUT = 36
+from jpeg_sig import *
+ 
 clock = Signal(bool(0))
-clkInOut = Signal(bool(0))
-ctn = Signal(intbv(0)[3:])
-pp0 = Signal(intbv(0)[WIDTH_OUT:])
-ld = Signal(bool(0))
-ld_o = Signal(bool(0)) 
-ss0 = Signal(bool(0)) 
+ctn1 = Signal(intbv(0)[6:]) 
 def cliparse():
     parser = argparse.ArgumentParser()
     parser.add_argument("--build", default=False, action='store_true')
@@ -20,20 +15,51 @@ def cliparse():
     return args
 def top_sending(clock,clkInOut,ss0, ld_o):
  
+
+    @always(clkInOut.posedge)
+    def rtl1():
+        if(ctn1 == 0):
+            ld.next = 1
+        else:
+            ld.next = 0
+    
+    @always(clkInOut.posedge)
+    def rtl():
+	if (ctn1 == 38):
+            ctn1.next = 0
+            
+        else:
+            ctn1.next = ctn1 + 1
+            
+               
      
     instance_1 = div_4(clock,clkInOut,ctn)
     instance_2 = para2ser(clkInOut, pp0, ss0, ld,ld_o)
     return instances()
-def tb(clock,clkInOut,ctn,pp0,ss0,ld):
+def tb(clock,clkInOut,ctn,pp0,ss0,ld_o):
+    @always(clkInOut.posedge)
+    def rtl1():
+        if(ctn1 == 0):
+            ld.next = 1
+        else:
+            ld.next = 0
+
+    @always(clkInOut.posedge)
+    def rtl():
+	if (ctn1 == 38):
+            ctn1.next = 0
+            
+        else:
+            ctn1.next = ctn1 + 1
     instance_1 = div_4(clock,clkInOut,ctn)
-    instance_2 = para2ser(clkInOut, pp0, ss0, ld)
+    instance_2 = para2ser(clkInOut, pp0, ss0, ld,ld_o)
+    
     @always(delay(10))
     def clkgen():
 	clock.next = not clock
     @instance
     def stimulus():
-        ctn.next = 0
-        yield clock.posedge
+         
         pp0.next = 164 << 27
         yield clock.posedge
         
@@ -49,7 +75,7 @@ def tb(clock,clkInOut,ctn,pp0,ss0,ld):
  
         ld.next = 0
         yield clkInOut.posedge
-        for i in range(4*40):
+        for i in range(6*40):
             yield clock.posedge
              
         raise StopSimulation	
@@ -61,7 +87,7 @@ def convert(args):
 def main():
     args = cliparse()
     if args.test:
-       tb_fsm = traceSignals(tb,clock,clkInOut,ctn,pp0,ss0,ld,ld_o)
+       tb_fsm = traceSignals(tb,clock,clkInOut,ctn,pp0,ss0,ld_o)
        sim = Simulation(tb_fsm)
        sim.run()  
     if args.convert:
