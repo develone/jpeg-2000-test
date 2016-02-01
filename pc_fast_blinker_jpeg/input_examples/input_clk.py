5c19705783b72d3972424fc9ebbb074dfd2e0edd
develone <develone@sbcglobal.net>
2016-02-01 08:32:04 -0700

    testing adding xbuild options to generate for xula2

--- pc_fast_blinker_jpeg/input_examples/input_clk.py
+++ pc_fast_blinker_jpeg/input_examples/input_clk.py
@@ -1,6 +1,7 @@
 from myhdl import *
 from ice40_primitives import *
 import argparse
+clock = Signal(bool(0))
 pi_in = Signal(bool(0))
 cat_out = Signal(bool(0))
 pi_in1 = Signal(bool(0))
@@ -11,6 +12,7 @@ pi_in3 = Signal(bool(0))
 cat_out3 = Signal(bool(0))
 def cliparse():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--xbuild", default=False, action='store_true')
     parser.add_argument("--build", default=False, action='store_true')
     parser.add_argument("--test", default=False, action='store_true')
     parser.add_argument("--convert", default=False, action='store_true')
@@ -29,7 +31,7 @@ def tb(pi_in,cat_out):
 	return instances()
 
 def convert(args):
-    toVerilog(cat_top,pi_in,cat_out,pi_in1,cat_out1,pi_in2,cat_out2,pi_in3,cat_out3)
+    toVerilog(cat_top,clock,pi_in,cat_out,pi_in1,cat_out1,pi_in2,cat_out2,pi_in3,cat_out3)
 
 def build(args):
 	import rhea.build as build
@@ -58,7 +60,47 @@ def build(args):
 		flow = brd.get_flow(top=cat_top)
 		flow.run()
 	run_catboard()
-def cat_top(pi_in,cat_out,pi_in1,cat_out1,pi_in2,cat_out2,pi_in3,cat_out3):
+def xbuild(args):
+        from pprint import pprint 
+	import rhea.build as build
+	from rhea.build.boards import get_board
+	#from input_clk import cat_in:
+
+
+	def run_xula():
+		brd = get_board('xula2')
+                brd.device = 'XC6SLX9'
+                
+		#LED1
+                #CHAN0
+                brd.add_port('cat_out', 'R7', pullup=True)
+		#brd.add_port('cat_out', 'A9')
+		#LED2
+                #CHAN22
+                brd.add_port('cat_out1', 'H1', pullup=True)
+		#brd.add_port('cat_out1', 'B8')
+		#LED3
+                #CHAN23
+                brd.add_port('cat_out2', 'H2', pullup=True)
+		#brd.add_port('cat_out2', 'A7')
+		#LED4
+                #CHAN19
+                brd.add_port('cat_out3', 'M1', pullup=True)
+		#brd.add_port('cat_out3', 'B7')
+		#BCM23
+		brd.add_port('pi_in', 'P9')
+		#BCM19
+		brd.add_port('pi_in1', 'T3')
+		#BCM24
+		brd.add_port('pi_in2', 'T9')
+		#BCM20
+		brd.add_port('pi_in3', 'R3')
+		flow = brd.get_flow(top=cat_top)
+		flow.run()
+                info = flow.get_utilization()
+                pprint(info)
+	run_xula()
+def cat_top(clock,pi_in,cat_out,pi_in1,cat_out1,pi_in2,cat_out2,pi_in3,cat_out3):
 	instance_1 = cat_in(pi_in,cat_out)
 	instance_2 = cat_in(pi_in1,cat_out1)
 	instance_3 = cat_in(pi_in2,cat_out2)
@@ -89,5 +131,7 @@ def main():
         convert(args)
     if args.build:
 		build(args)
+    if args.xbuild:
+        xbuild(args)
 if __name__ == '__main__':
     main() 
