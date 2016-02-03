a39f314c8fe423c087e59149b11922788e4f3935
develone <develone@sbcglobal.net>
2016-02-02 18:21:38 -0700

    still having problem with de_interleave Traceback (most recent call last):   File "test_dwt_top.py", line 136, in <module>     de_interleave(m,h,w)   File "test_dwt_top.py", line 47, in de_interleave     temp_bank[col][row/2] =  m[row][col] TypeError: list indices must be integers, not float python test_top_a.py --test wks okay

--- pc_fast_blinker_jpeg/test_dwt_top.py
+++ pc_fast_blinker_jpeg/test_dwt_top.py
@@ -36,78 +36,75 @@ def _prep_cosim(args, **sigs):
     print("  *%s" %  (cmd))
     return Cosimulation(cmd, **sigs)
 
+def de_interleave(m,height,width):
+    # de-interleave
+    temp_bank = [[0]*width for i in range(height)]
+    for row in range(width):
+        for col in range(width):
+            # k1 and k2 scale the vals
+            # simultaneously transpose the matrix when deinterleaving
+            if row % 2 == 0:
+                temp_bank[col][row/2] =  m[row][col]
+            else:
+                temp_bank[col][row/2 + height/2] =  m[row][col]
+    # write temp_bank to s:
+    for row in range(width):
+        for col in range(height):
+            m[row][col] = temp_bank[row][col]
+
+def seq_to_img(m, pix):
+    ''' Copy matrix m to pixel buffer pix.
+    Assumes m has the same number of rows and cols as pix. '''
+    for row in range(len(m)):
+        for col in range(len(m[row])):
+            pix[col,row] = m[row][col]
+
+from PIL import Image
+im = Image.open("../lena_256.png")
+w, h = im.size
+pix = im.load()
+m = list(im.getdata())
+m = [m[i:i+im.size[0]] for  i in range(0, len(m), im.size[0])]
 
 def test_dwt_top(args):
-    def de_interleave(m,height,width):
-        # de-interleave
-        temp_bank = [[0]*width for i in range(height)]
-        for row in range(width):
-            for col in range(width):
-                # k1 and k2 scale the vals
-                # simultaneously transpose the matrix when deinterleaving
-                if row % 2 == 0:
-    
-                    temp_bank[col][row/2] =  m[row][col]
-                else:
-    
-                    temp_bank[col][row/2 + height/2] =  m[row][col]
-        # write temp_bank to s:
-        for row in range(width):
-            for col in range(height):
-                m[row][col] = temp_bank[row][col]
-    def seq_to_img(m, pix):
-        ''' Copy matrix m to pixel buffer pix.
-        Assumes m has the same number of rows and cols as pix. '''
-        for row in range(len(m)):
-            for col in range(len(m[row])):
-                pix[col,row] = m[row][col]
-    from PIL import Image
-    im = Image.open("../lena_256.png")
-    pix = im.load()
-    m = list(im.getdata())
-    #print m.__sizeof__()
-    m = [m[i:i+im.size[0]] for  i in range(0, len(m), im.size[0])]
+
     tbdut =  _prep_cosim(args,flgs0=flgs0,upd0=upd0,lft0=lft0,sam0=sam0,rht0=rht0,done0=done0,clock=clock,z0=z0,flgs1=flgs1,upd1=upd1,lft1=lft1,sam1=sam1,rht1=rht1,done1=done1,z1=z1 )
 
-    @always(delay(10))
+    @always(delay(4))
     def tbclk():
        clock.next = not clock
     
     @instance
     def tbstim():
         for col in range(256):
-            for row in range(6,256,2):
-                #1 2 3  0 1 2
-                # 1
-                lft0.next = m[row-5][col]
+            for row in range(2,256,2):
+ 
+                lft0.next = m[row-1][col]
                 yield clock.posedge
-                # 2
-                sam0.next = m[row-4][col]
+                
+                sam0.next = m[row][col]
                 yield clock.posedge
-                # 3
-                rht0.next = m[row-3][col]
+                
+                rht0.next = m[row+1][col]
                 yield clock.posedge
-                #print ("%8d: %d %d %d " % (now(),lft0,sam0,rht0))
+                 
                 flgs0.next = 7
                 yield clock.posedge
                 upd0.next = 1
                 yield clock.posedge
                 upd0.next = 0
                 yield clock.posedge
-                #this needs to be the rht1
-                #1 2 3  0 1 2
-                # 2
-                m[row-4][col] = lift0[W0:]
-                
-                # 2
-                rht1.next = z0
-                yield clock.posedge
-                #print ("%8d: %d " % (now(),rht1))
-                # 0
-                lft1.next = m[row-6][col]
+ 
+                #print ("%8d: %d %d %d %d %d %d" % (now(),row,col,lft0,sam0,rht0,z0))
+                m[row][col] = int(z0)
+ 
+            for row in range(1,256-1,2):
+                lft1.next = m[row-1][col]
                 yield clock.posedge
                 # 1
-                sam1.next = lft0
+                sam1.next = m[row][col]
+                yield clock.posedge
+                rht1.next = m[row+1][col]
                 yield clock.posedge
                 flgs1.next = 6
                 yield clock.posedge
@@ -115,19 +112,21 @@ def test_dwt_top(args):
                 yield clock.posedge
                 upd1.next = 0
                 yield clock.posedge
-                #this needs to be the rht2
-                # 1
-                m[row-5][col] = lift1[W0:]
-                #print ("%8d: %d " % (now(),lift1[W0:])) 
-        de_interleave(m,256,256)
+ 
+                #print ("%8d: %d %d %d %d %d %d" % (now(),row,col,lft1,sam1,rht1,z1))
+                m[row][col] = int(z1)
+        print ("8%d :%d %d" % (now(),h,w))
+        #de_interleave(m,h,w)
         seq_to_img(m, pix)
         print("%8d: writing test1_256_fwt1pass.png " % (now()))
         im.save("test1_256_fwt1pass.png")
+      
         raise StopSimulation
 
     print("start (co)simulation ...")
     Simulation((tbdut, tbstim, tbclk,)).run()
 
 
-if __name__ == '__main__':
-    test_dwt_top(Namespace())
+test_dwt_top(Namespace())
+ 
+
