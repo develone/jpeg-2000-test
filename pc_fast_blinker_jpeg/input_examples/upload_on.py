58763bd96ac6c9ff87a91ff9161304beba98e127
develone <develone@sbcglobal.net>
2016-02-09 18:20:33 -0700

    now getting the reply also

--- pc_fast_blinker_jpeg/input_examples/upload_on.py
+++ pc_fast_blinker_jpeg/input_examples/upload_on.py
@@ -4,8 +4,9 @@ data = file.read(12)
 ser = serial.Serial ("/dev/ttyAMA0")    
 ser.baudrate = 115200                      
 
-print data
+#print data
 ser.write(data) 
- 
+reply = ser.read(12) 
+print "this is the reply", reply 
 file.close
 
