868e6075769f2584f6a515875063b8e6b391f8f4
develone <develone@sbcglobal.net>
2016-02-09 17:23:02 -0700

    python catboard_blinky_host.py --build creates iceriver/catboard.v & iceriver/catboard.pcf
    sudo config_cat catboard_bitmap.bin
    generated in windows 8 using iCEcube2
    with minicom
    
    minicom -8 -H -b 115200  -o -D /dev/ttyAMA0
    The files were genrated with python wr_bin_on.py
    & python wr_bin_off.py
    
    ascii upload the file led_on.bin to turn the on leds
    od -x led_on.bin
    0000000 02de 0000 2000 ca04 0000 ff00
    0000014
    
    ascii upload the file led_off.bin to turn off the leds an blink at 1 per sec
    od -x led_off.bin
    0000000 02de 0000 2000 ca04 0000 0000
    0000014

--- pc_fast_blinker_jpeg/input_examples/catboard_blinky_host.py
+++ pc_fast_blinker_jpeg/input_examples/catboard_blinky_host.py
@@ -4,7 +4,6 @@ import subprocess
 
 from myhdl import (Signal, intbv, always_seq, always_comb, concat,)
 
-
 from rhea.cores.uart import uartlite
 from rhea.cores.memmap import memmap_command_bridge
 from rhea.cores.misc import glbl_timer_ticks
@@ -16,16 +15,8 @@ from rhea.build.boards import get_board
 
 def catboard_blinky_host(clock, led, uart_tx, uart_rx):
     """
-    This example is similar to the other examples in this directory but
-    the LEDs are controlled externally via command packets sent from a
-    host via the UART on the catboard.
-
-    Ports:
-      clock:
-      led:
-      pmod:
-      uart_tx:
-      uart_rx:
+    The LEDs are controlled from the RPi over the UART
+    to the FPGA.
     """
 
     glbl = Global(clock, None)
@@ -42,7 +33,9 @@ def catboard_blinky_host(clock, led, uart_tx, uart_rx):
     memmap = Barebone(glbl, data_width=32, address_width=32)
 
     # create the UART instance.
-    uart_inst = uartlite(glbl, fbustx, fbusrx, uart_rx, uart_tx)
+    uart_inst = uartlite(glbl, fbustx, fbusrx,
+                         serial_in=uart_rx,
+                         serial_out=uart_tx)
 
     # create the packet command instance
     cmd_inst = memmap_command_bridge(glbl, fbusrx, fbustx, memmap)
@@ -69,26 +62,20 @@ def catboard_blinky_host(clock, led, uart_tx, uart_rx):
             tone.next = (~tone) & 0x1
         led.next = ledreg | tone[5:] 
             
-        #pmod.next = 0
-
-    # @todo: PMOD OLED memmap control
-
     return (tick_inst, uart_inst, cmd_inst, 
             beh_led_control, beh_led_read, beh_assign)
 
 
 def build(args):
     brd = get_board('catboard')
-    #BCM15
-    brd.add_port('uart_rx', 'T14')
-    #BCM14
-    brd.add_port('uart_tx', 'T15')
+    brd.add_port_name('uart_rx', 'bcm14_txd')                           
+    brd.add_port_name('uart_tx', 'bcm15_rxd')
     flow = brd.get_flow(top=catboard_blinky_host)
     flow.run()
 
 
 def program(args):
-    subprocess.check_call(["iceprog", "iceriver/catboard.bin"])
+    subprocess.check_call(["iceprog", "iceriver/icestick.bin"])
 
 
 def cliparse():
@@ -101,16 +88,20 @@ def cliparse():
     return args
 
 
+def test_instance():    
+    # check for basic syntax errors, use test_ice* to test
+    # functionality
+    catboard_blinky_host(
+        clock=Clock(0, frequency=50e6),
+        led=Signal(intbv(0)[8:]), 
+        uart_tx=Signal(bool(0)),
+        uart_rx=Signal(bool(0)), )
+
+    
 def main():
     args = cliparse()
     if args.test:
-        # check for basic syntax errors, use test_ice* to test
-        # functionality
-        catboard_blinky_host(
-            clock=Clock(0, frequency=100e6),
-            led=Signal(intbv(0)[8:]), 
-            uart_tx=Signal(bool(0)),
-            uart_rx=Signal(bool(0)) )
+        test_instance()
         
     if args.build:
         build(args)
---
+++ pc_fast_blinker_jpeg/input_examples/wr_bin_on.py
@@ -0,0 +1,15 @@
+'''
+python wr_bin_on.py will create a file
+led_on.bin which can be uploaded to ICE40 FPGA
+
+od -x led_on.bin 
+0000000 02de 0000 2000 ca04 0000 ff00
+0000014
+'''
+
+file = open("led_on.bin","wb")
+file.write("\xde\x02\x00\x00\x00\x20\x04\xca\x00\x00\x00\xff")
+file.close
+ 
+
+
