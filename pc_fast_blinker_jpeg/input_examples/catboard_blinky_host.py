18488e3e978c720987fd2360f87d96194b983a10
develone <develone@sbcglobal.net>
2016-02-23 13:13:34 -0700

    chg'ed several always_seq to always beh_led_control, beh_assign, beh_my_ret_reg, and beh_my_registers clean by removing the Signals using from uart_sig import * instead
    Verilog file created on pi@raspberrypi2-152
    
    Copied to Windows 8 system.
    
    built with iCECube2
    
    After the build with iCECube2 transfer to iceriver.
    
    cp /cygdrive/c/lscc/iCEcube2.2015.08//sbt_backend/Projects/uart_test/uart_test_Implmnt/sbt/outputs/bitmap/catboard_bitmap.bin iceriver/
    
    Set the premission to transfer file
    
    chmod +r iceriver/catboard_bitmap.bin
    
    Transferred to ws009
    scp iceriver/catboard_bitmap.bin 192.168.1.123:~/iCECube2022316/
    
    Program ICE40_HX8K
    
    sudo config_cat ~/iCECube2022316/catboard_bitmap.bin
    
    Send samples to ICE40_HX8K
    pi@mysshserver ~/jpeg-2000-test/pc_fast_blinker_jpeg/input_examples $
    
    python test_dwt_img.py
    pi@mysshserver ~/jpeg-2000-test/pc_fast_blinker_jpeg/input_examples $
    od -x data_from_fpga.bin
    0000000 02de 0000 0000 ca04 0000 0000 02de 0000
    0000020 0400 ca04 0000 0000 02de 0000 0800 ca04
    0000040 0000 0000 02de 0000 0c00 ca04 0000 0000
    0000060 02de 0000 1000 ca04 0000 0000 02de 0000
    0000100 1400 ca04 0000 0000 02de 0000 1800 ca04
    0000120 0000 0000 02de 0000 1c00 ca04 0000 0000
    0000140 02de 0000 2000 ca04 0000 0000 02de 0000
    0000160 2400 ca04 0000 0000 02de 0000 2800 ca04
    0000200 fc01 0000 02de 0000 2c00 ca04 fc01 fc01
    0000220 02de 0000 3000 ca04 0000 0000 02de 0000
    0000240 4000 ca04 0000 0000 02de 0000 0000 ca04
    0000260 0000 0000 02de 0000 0400 ca04 0000 0000
    0000300 02de 0000 0800 ca04 0000 0000 02de 0000
    0000320 0c00 ca04 0000 0000 02de 0000 1000 ca04
    0000340 0000 0000 02de 0000 1400 ca04 0000 0000
    0000360 02de 0000 1800 ca04 0000 0000 02de 0000
    0000400 1c00 ca04 0000 0000 02de 0000 2000 ca04
    0000420 0000 0000 02de 0000 2400 ca04 0000 0000
    0000440 02de 0000 4000 ca04 f400 f400
    0000454

--- pc_fast_blinker_jpeg/input_examples/catboard_blinky_host.py
+++ pc_fast_blinker_jpeg/input_examples/catboard_blinky_host.py
@@ -15,100 +15,8 @@ from jpeg import dwt
 from signed2twoscomplement import signed2twoscomplement
 from l2r import lift2res1
 from sh_reg import toSig
-
-WIDTH_OUT = 36
-WIDTH = 31
-W0 = 9
-upd0 = Signal(bool(0))
-upd1 = Signal(bool(0))
-z0 = Signal(intbv(0)[W0:])
-z1 = Signal(intbv(0)[W0:])
-done0 = Signal(bool(0))
-done1 = Signal(bool(0))
-res0 = Signal(intbv(0, min=-(2**(W0)), max=(2**(W0))))
-res1 = Signal(intbv(0, min=-(2**(W0)), max=(2**(W0))))
-flgs0 = Signal(intbv(0)[3:])
-flgs1 = Signal(intbv(0)[3:])
-lft0 = Signal(intbv(0)[W0:])
-lft1 = Signal(intbv(0)[W0:])
-sam0 = Signal(intbv(0)[W0:])
-sam1 = Signal(intbv(0)[W0:])
-rht0 = Signal(intbv(0)[W0:])
-rht1 = Signal(intbv(0)[W0:])
-lift0 = Signal(intbv(0, min=-(2**(W0)), max=(2**(W0))))
-lift1 = Signal(intbv(0, min=-(2**(W0)), max=(2**(W0))))
-
-upd2 = Signal(bool(0))
-upd3 = Signal(bool(0))
-z2 = Signal(intbv(0)[W0:])
-z3 = Signal(intbv(0)[W0:])
-done2 = Signal(bool(0))
-done3 = Signal(bool(0))
-res2 = Signal(intbv(0, min=-(2**(W0)), max=(2**(W0))))
-res3 = Signal(intbv(0, min=-(2**(W0)), max=(2**(W0))))
-flgs2 = Signal(intbv(0)[3:])
-flgs3 = Signal(intbv(0)[3:])
-lft2 = Signal(intbv(0)[W0:])
-lft3 = Signal(intbv(0)[W0:])
-sam2 = Signal(intbv(0)[W0:])
-sam3 = Signal(intbv(0)[W0:])
-rht2 = Signal(intbv(0)[W0:])
-rht3 = Signal(intbv(0)[W0:])
-lift2 = Signal(intbv(0, min=-(2**(W0)), max=(2**(W0))))
-lift3 = Signal(intbv(0, min=-(2**(W0)), max=(2**(W0))))
-
-upd4 = Signal(bool(0))
-upd5 = Signal(bool(0))
-z4 = Signal(intbv(0)[W0:])
-z5 = Signal(intbv(0)[W0:])
-done4 = Signal(bool(0))
-done5 = Signal(bool(0))
-res4 = Signal(intbv(0, min=-(2**(W0)), max=(2**(W0))))
-res5 = Signal(intbv(0, min=-(2**(W0)), max=(2**(W0))))
-flgs4 = Signal(intbv(0)[3:])
-flgs5 = Signal(intbv(0)[3:])
-lft4 = Signal(intbv(0)[W0:])
-lft5 = Signal(intbv(0)[W0:])
-sam4 = Signal(intbv(0)[W0:])
-sam5 = Signal(intbv(0)[W0:])
-rht4 = Signal(intbv(0)[W0:])
-rht5 = Signal(intbv(0)[W0:])
-lift4 = Signal(intbv(0, min=-(2**(W0)), max=(2**(W0))))
-lift5 = Signal(intbv(0, min=-(2**(W0)), max=(2**(W0))))
-
-upd6 = Signal(bool(0))
-upd7 = Signal(bool(0))
-z6 = Signal(intbv(0)[W0:])
-z7 = Signal(intbv(0)[W0:])
-done6 = Signal(bool(0))
-done7 = Signal(bool(0))
-res6 = Signal(intbv(0, min=-(2**(W0)), max=(2**(W0))))
-res7 = Signal(intbv(0, min=-(2**(W0)), max=(2**(W0))))
-flgs6 = Signal(intbv(0)[3:])
-flgs7 = Signal(intbv(0)[3:])
-lft6 = Signal(intbv(0)[W0:])
-lft7 = Signal(intbv(0)[W0:])
-sam6 = Signal(intbv(0)[W0:])
-sam7 = Signal(intbv(0)[W0:])
-rht6 = Signal(intbv(0)[W0:])
-rht7 = Signal(intbv(0)[W0:])
-lift6 = Signal(intbv(0, min=-(2**(W0)), max=(2**(W0))))
-lift7 = Signal(intbv(0, min=-(2**(W0)), max=(2**(W0))))
-
-
-myregister0 = Signal(intbv(0)[32:])
-myregister1 = Signal(intbv(0)[32:])
-myregister2 = Signal(intbv(0)[32:])
-myregister3 = Signal(intbv(0)[32:])
-myregister4 = Signal(intbv(0)[32:])
-myregister5 = Signal(intbv(0)[32:])
-myregister6 = Signal(intbv(0)[32:])
-myregister7 = Signal(intbv(0)[32:])
+from uart_sig import *
  
-data_to_host0 = Signal(intbv(0)[32:])
-data_to_host1 = Signal(intbv(0)[32:])
-data_to_host2 = Signal(intbv(0)[32:])
-data_to_host3 = Signal(intbv(0)[32:])
 def catboard_blinky_host(clock, reset, led, uart_tx, uart_rx):
     """
     The LEDs are controlled from the RPi over the UART
@@ -138,7 +46,7 @@ def catboard_blinky_host(clock, reset, led, uart_tx, uart_rx):
 
  
 
-    @always_seq(clock.posedge, reset=None)
+    @always(clock.posedge)
     def beh_led_control():
         memmap.done.next = not (memmap.write or memmap.read)
         if memmap.write and memmap.mem_addr == 0x40:
@@ -155,7 +63,7 @@ def catboard_blinky_host(clock, reset, led, uart_tx, uart_rx):
     # blink one of the LEDs
     tone = Signal(intbv(0)[8:])
 
-    @always_seq(clock.posedge, reset=None)
+    @always(clock.posedge)
     def beh_assign():
         if glbl.tick_sec:
             tone.next = (~tone) & 0x1
@@ -203,7 +111,7 @@ def catboard_blinky_host(clock, reset, led, uart_tx, uart_rx):
     inst_sig6 = toSig(clock, myregister6,flgs6,lft6,sam6,rht6) 
     inst_sig7 = toSig(clock, myregister7,flgs7,lft7,sam7,rht7) 
 
-    @always_seq(clock.posedge, reset=None) 
+    @always(clock.posedge) 
     def beh_my_ret_reg():
         if memmap.read:
             if (memmap.mem_addr == 36):
@@ -214,7 +122,7 @@ def catboard_blinky_host(clock, reset, led, uart_tx, uart_rx):
                 memmap.read_data.next = data_to_host2
             if (memmap.mem_addr == 48):
                 memmap.read_data.next = data_to_host3  
-    @always_seq(clock.posedge, reset=None) 
+    @always(clock.posedge) 
     def beh_my_registers():
         if memmap.write:
             if memmap.mem_addr == 0:
