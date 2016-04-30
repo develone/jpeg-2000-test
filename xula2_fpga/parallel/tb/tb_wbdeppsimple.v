3d85ee5c3081cf47dff3a519e21f600803104b51
vidal <vidal@vidal-MX6438.(none)>
2016-04-30 13:23:20 -0600

    going back to co-sim now started generating vcd file. ver c86f35e65e941f020

--- xula2_fpga/parallel/tb/tb_wbdeppsimple.v
+++ xula2_fpga/parallel/tb/tb_wbdeppsimple.v
@@ -4,38 +4,21 @@ module tb_wbdeppsimple1;
 	reg i_dstb_n;
 	reg i_write_n;
 	reg [7:0] i_depp;
-	reg [7:0] o_depp;
 	reg i_wb_ack;
 	reg i_wb_stall;
 	reg i_wb_err;
 	reg [31:0] i_wb_data;
 	reg i_int;
-	
-	reg i_b0;
-	reg i_b1;
-	reg i_b2;
-	reg i_b3;
-	reg i_b4;
-	reg i_b5;
-	reg i_b6;
-	reg i_b7;
 
 	// Outputs
-	//wire [7:0] o_depp;
+	wire [7:0] o_depp;
 	wire o_wait;
 	wire o_wb_cyc;
 	wire o_wb_stb;
 	wire o_wb_we;
 	wire [31:0] o_wb_addr;
 	wire [31:0] o_wb_data;
-	reg o_b0;
-	reg o_b1;
-	reg o_b2;
-	reg o_b3;
-	reg o_b4;
-	reg o_b5;
-	reg o_b6;
-	reg o_b7;
+
 initial begin
     $dumpfile("tb/vcd/wbdeppsimple1.vcd");
     $dumpvars(0, tb_wbdeppsimple1);
@@ -50,29 +33,12 @@ initial begin
         i_dstb_n,
         i_write_n,
         i_depp,
-        o_depp,
+ 
         i_wb_ack,
         i_wb_stall,
         i_wb_err,
         i_wb_data,
-        i_int,
-        i_b0,
-        i_b1,
-        i_b2,
-        i_b3,
-        i_b4,
-        i_b5,
-        i_b6,
-        i_b7,
-        o_b0,
-        o_b1,
-        o_b2,
-        o_b3,
-        o_b4,
-        o_b5,
-        o_b6,
-        o_b7        
-        
+        i_int
     );
     $to_myhdl(
         o_depp,
@@ -82,7 +48,6 @@ initial begin
         o_wb_data,
         o_wb_cyc,
         o_wb_stb
-
     );
     
 end
@@ -104,5 +69,5 @@ wbdeppsimple dut_wbdeppsimple(i_clk,
 	i_wb_err,
 	i_wb_data,
 	i_int);
- 
+
 endmodule
