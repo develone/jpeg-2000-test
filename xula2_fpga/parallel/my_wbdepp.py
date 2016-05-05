e76a904e4bd601176d8984efc392ce0913d37511
vidal <vidal@vidal-MX6438.(none)>
2016-05-05 14:14:35 -0600

    wkg on trying to get delay working

--- xula2_fpga/parallel/my_wbdepp.py
+++ xula2_fpga/parallel/my_wbdepp.py
@@ -2,8 +2,9 @@ import myhdl
 from myhdl import *
 import argparse
 from argparse import Namespace
-
-from dr_wbdepp import rpi2B_io
+from rhea.system import Reset
+reset = Reset(0, active=1, async=False)
+#from dr_wbdepp import rpi2B_io
 
 i_rpi2B = Signal(intbv(0)[8:])
 o_rpi2B = Signal(intbv(0)[8:])
@@ -46,6 +47,35 @@ astb = Signal(bool(0))
 dstb = Signal(bool(0))
 w_write = Signal(bool(0))
 addr = Signal(intbv(0)[8:])
+def rpi2B_io(i_clk,i_rpi2B,fr_depp,o_rpi2B,to_depp):
+ 
+
+
+		
+ 
+    @always(i_clk.posedge)
+    def rtl1():
+        to_depp[1:0].next = i_rpi2B[1:0]
+        to_depp[2:1].next = i_rpi2B[2:1]
+        to_depp[3:2].next = i_rpi2B[3:2]
+        to_depp[4:3].next = i_rpi2B[4:3]
+        to_depp[5:4].next = i_rpi2B[5:4]
+        to_depp[6:5].next = i_rpi2B[6:5]
+        to_depp[7:6].next = i_rpi2B[7:6]
+        to_depp[8:7].next = i_rpi2B[8:7]
+    @always(i_clk.posedge)
+    def rtl2():
+        o_rpi2B[1:0].next = fr_depp[1:0]
+        o_rpi2B[2:1].next = fr_depp[2:1]
+        o_rpi2B[3:2].next = fr_depp[3:2]
+        o_rpi2B[4:3].next = fr_depp[4:3]
+        o_rpi2B[5:4].next = fr_depp[5:4]
+        o_rpi2B[6:5].next = fr_depp[6:5]
+        o_rpi2B[7:6].next = fr_depp[7:6]
+        o_rpi2B[8:7].next = fr_depp[8:7]
+
+				        	
+    return myhdl.instances()
 def cliparse():
     parser = argparse.ArgumentParser()
     parser.add_argument("--brd", default='xula2_stickit_mb')
@@ -71,7 +101,7 @@ o_wb_we,o_wb_addr,o_wb_data,i_wb_ack,i_wb_stall,i_wb_err,i_wb_data,i_int):
     def rtl1():        
         r_dstb_n.next = x_dstb_n
         r_astb_n.next = x_astb_n
-        #r_write_n = x_write_n
+        r_write_n = x_write_n
         r_depp.next = x_depp
       
     @always(i_clk.posedge)
@@ -98,19 +128,41 @@ o_wb_we,o_wb_addr,o_wb_data,i_wb_ack,i_wb_stall,i_wb_err,i_wb_data,i_int):
  
     '''    			    		       
     return myhdl.instances()
-def toplevel(i_clk,i_astb_n,i_dstb_n,i_write_n,to_depp,fr_depp,o_wait,o_wb_cyc,o_wb_stb,o_wb_we, \
-o_wb_addr,o_wb_data,i_wb_ack,i_wb_stall,i_wb_err,i_wb_data,i_int, \
-i_rpi2B, o_rpi2B ):
-    dut_rpi2B_io = rpi2B_io(i_rpi2B,fr_depp,o_rpi2B,to_depp)
+def toplevel(i_clk, i_rpi2B,fr_depp,o_rpi2B,to_depp):
+    dut_rpi2B_io = rpi2B_io(i_clk,i_rpi2B,fr_depp,o_rpi2B,to_depp)
+    reset_dly_cnt = Signal(intbv(0)[5:])
+    @always(i_clk.posedge)
+    def reset_tst():
+        '''
+        For the first 4 clocks the reset is forced to lo
+        for clock 6 to 31 the reset is set hi
+        then the reset is lo
+        '''
+        if (reset_dly_cnt < 31):
+            reset_dly_cnt.next = reset_dly_cnt + 1
+            if (reset_dly_cnt <= 4):
+                reset.next = 0
+            if (reset_dly_cnt >= 5):
+                reset.next = 1
+                #i_int.next = 1
+        else:
+            reset.next = 0
+            '''
+            x_dstb_n.next = 1
+            r_dstb_n.next = 1
+            l_dstb_n.next = 1
+            x_astb_n.next = 1
+            r_astb_n.next = 1
+            l_astb_n.next = 1
+            i_int.next = 0'''
 
-    dut_my_wbdepp = my_wbdepp(i_clk,i_astb_n,i_dstb_n,i_write_n,i_depp,o_depp,o_wait,o_wb_cyc,o_wb_stb, \
-o_wb_we,o_wb_addr,o_wb_data,i_wb_ack,i_wb_stall,i_wb_err,i_wb_data,i_int)    
+    dut_my_wbdepp = my_wbdepp(i_clk,i_astb_n,i_dstb_n,i_write_n,to_depp,\
+    fr_depp,o_wait,o_wb_cyc,o_wb_stb, \
+    o_wb_we,o_wb_addr,o_wb_data,i_wb_ack,i_wb_stall,i_wb_err,i_wb_data,i_int)    
     return myhdl.instances()
 
-def tb(i_clk,i_astb_n,i_dstb_n,i_write_n,to_depp,fr_depp,o_wait,o_wb_cyc,o_wb_stb,o_wb_we, \
-o_wb_addr,o_wb_data,i_wb_ack,i_wb_stall,i_wb_err,i_wb_data,i_int, \
-i_rpi2B, o_rpi2B ):
-    dut_rpi2B_io = rpi2B_io(i_rpi2B,fr_depp,o_rpi2B,to_depp)
+def tb(i_clk, i_rpi2B,fr_depp,o_rpi2B,to_depp ):
+    dut_rpi2B_io = toplevel(i_clk,i_rpi2B,fr_depp,o_rpi2B,to_depp)
     dut_my_wbdepp = my_wbdepp(i_clk,i_astb_n,i_dstb_n,i_write_n,to_depp,fr_depp,o_wait,o_wb_cyc,o_wb_stb, \
 o_wb_we,o_wb_addr,o_wb_data,i_wb_ack,i_wb_stall,i_wb_err,i_wb_data,i_int)
 
@@ -120,6 +172,7 @@ o_wb_we,o_wb_addr,o_wb_data,i_wb_ack,i_wb_stall,i_wb_err,i_wb_data,i_int)
 
     @instance
     def stimulus():
+       yield delay(1000)	
        yield i_clk.posedge
        i_write_n.next = 1
        yield i_clk.posedge
@@ -130,10 +183,11 @@ o_wb_we,o_wb_addr,o_wb_data,i_wb_ack,i_wb_stall,i_wb_err,i_wb_data,i_int)
        
        '''writing addres 01020304 1st byte'''
        
-       i_depp.next = 1
+       i_rpi2B.next = 1
        yield i_clk.posedge
        
        yield i_clk.posedge
+       
 
 
 
@@ -154,7 +208,7 @@ o_wb_we,o_wb_addr,o_wb_data,i_wb_ack,i_wb_stall,i_wb_err,i_wb_data,i_int)
        while(o_wait):
            print "wait for o_wait",o_wait
            yield i_clk.posedge
-       i_depp.next = 2
+       i_rpi2B.next = 2
        yield i_clk.posedge
        
        yield i_clk.posedge
@@ -178,7 +232,7 @@ o_wb_we,o_wb_addr,o_wb_data,i_wb_ack,i_wb_stall,i_wb_err,i_wb_data,i_int)
        while(o_wait):
            print "wait for o_wait",o_wait
            yield i_clk.posedge
-       i_depp.next = 3
+       i_rpi2B.next = 3
        yield i_clk.posedge
        
        yield i_clk.posedge
@@ -203,7 +257,7 @@ o_wb_we,o_wb_addr,o_wb_data,i_wb_ack,i_wb_stall,i_wb_err,i_wb_data,i_int)
            print "wait for o_wait",o_wait
            yield i_clk.posedge
 			   
-       i_depp.next = 4
+       i_rpi2B.next = 4
        yield i_clk.posedge
        
        yield i_clk.posedge
@@ -228,7 +282,7 @@ o_wb_we,o_wb_addr,o_wb_data,i_wb_ack,i_wb_stall,i_wb_err,i_wb_data,i_int)
            print "wait for o_wait",o_wait
            yield i_clk.posedge
 			    
-       '''no i_astb_n ''' 
+       '''no i_astb_n 
        i_depp.next = 1
        yield i_clk.posedge
        
@@ -301,7 +355,7 @@ o_wb_we,o_wb_addr,o_wb_data,i_wb_ack,i_wb_stall,i_wb_err,i_wb_data,i_int)
            print "wait for o_wait",o_wait
            yield i_clk.posedge
 			   
-       i_depp.next = 4
+       i_rpi2B.next = 4
        yield i_clk.posedge
        
        yield i_clk.posedge
@@ -324,22 +378,18 @@ o_wb_we,o_wb_addr,o_wb_data,i_wb_ack,i_wb_stall,i_wb_err,i_wb_data,i_int)
 
        while(o_wait):
            print "wait for o_wait",o_wait
-           yield i_clk.posedge
+           yield i_clk.posedge'''
        raise StopSimulation   		    
     return myhdl.instances()	    
 #toVerilog(my_wbdepp,i_clk,i_astb_n,i_dstb_n,i_write_n,i_depp,o_depp,o_wait,o_wb_cyc,o_wb_stb,o_wb_we,o_wb_addr,o_wb_data,i_wb_ack,i_wb_stall,i_wb_err,i_wb_data,i_int)
 #toVerilog(rpi2B_io,i_rpi2B,fr_depp,o_rpi2B,to_depp)
 def convert():
-	toVerilog(toplevel,i_clk,i_astb_n,i_dstb_n,i_write_n,to_depp,fr_depp,o_wait,o_wb_cyc, \
-o_wb_stb,o_wb_we,o_wb_addr,o_wb_data,i_wb_ack,i_wb_stall,i_wb_err,i_wb_data,i_int, \
-i_rpi2B,o_rpi2B)
+	toVerilog(toplevel,i_clk,i_rpi2B,fr_depp,o_rpi2B,to_depp)
  
 def main():
     args = cliparse()
     if args.trace:
-        tb_fsm = traceSignals(tb,i_clk,i_astb_n,i_dstb_n,i_write_n,to_depp,fr_depp,o_wait,o_wb_cyc,o_wb_stb,o_wb_we, \
-o_wb_addr,o_wb_data,i_wb_ack,i_wb_stall,i_wb_err,i_wb_data,i_int, \
-i_rpi2B, o_rpi2B )
+        tb_fsm = traceSignals(tb,i_clk,i_rpi2B,fr_depp,o_rpi2B,to_depp )
         sim = Simulation(tb_fsm)
         sim.run()
     if args.build:
