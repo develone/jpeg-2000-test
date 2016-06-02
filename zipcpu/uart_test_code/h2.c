7a076d13dd1dd8512db0328e331a8571bf7bb5e1
develone <develone@sbcglobal.net>
2016-06-02 14:13:44 -0600

    adding board.h

--- zipcpu/uart_test_code/h2.c
+++ zipcpu/uart_test_code/h2.c
@@ -1,3 +1,4 @@
+
 asm("\t.section\t.start\n"
 	"\t.global\t_start\n"
 	"\t.type\t_start,@function\n"
@@ -6,33 +7,12 @@ asm("\t.section\t.start\n"
 	"\tBRA\tentry\n"
 	"\t.section\t.text");
 
-#define	INT_RTC		0x002
-#define	INT_FLASH	0x004
-#define	INT_SCOPE	0x008
-#define	INT_GPIO	0x010
-#define	INT_PWM		0x020
-#define	INT_UARTRX	0x040
-#define	INT_UARTTX	0x080
-
-typedef	struct	{
-	volatile int		io_reserved, io_version, io_pic;
-	volatile unsigned	*io_buserr;
-	volatile unsigned	io_bustimer;
-	volatile unsigned	io_rtcdate;
-	volatile unsigned	io_gpio;
-	volatile unsigned	io_uart_ctrl;
-	volatile unsigned	io_pwm_audio, io_pwm_timer;
-	volatile unsigned	io_uart_rx, io_uart_tx;
-	volatile unsigned	io_flash_ereg, io_flash_config,
-				io_flash_status, io_flash_devid;
-	volatile unsigned	io_rtc_clock, io_rtc_timer, io_rtc_stopwatch,
-				io_rtc_alarm;
-} IOSPACE;
+#include "board.h" 
 
 const char msg[] = "Hello, world! testing 1000000 Baud RPi2B with zipcpu & xulalx25soc  \r\n";
 
 void entry(void) {
-	register IOSPACE	*sys = (IOSPACE *)0x0100;
+	//register IOSPACE	*sys = (IOSPACE *)0x0100;
 	int	counts = 0;
 
 	// Let's set ourselves up for 1000000 baud, 8-bit characters, no parity,
