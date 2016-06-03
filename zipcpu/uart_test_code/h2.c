6b8473baebd019ab7fbb89286f7ed0380c867334
vidal <vidal@vidal-MX6438.(none)>
2016-06-03 07:04:08 -0600

    adding another msg pointer

--- zipcpu/uart_test_code/h2.c
+++ zipcpu/uart_test_code/h2.c
@@ -9,31 +9,36 @@ asm("\t.section\t.start\n"
 
 #include "board.h" 
 
-const char msg[] = "Hello, world! testing 1000000 Baud RPi2B with zipcpu & xulalx25soc  \r\n";
-
+const char msg[] = "Hello, world!\r\n";
+const char msg1[] = "Data rdy\r\n";
 void entry(void) {
 	//register IOSPACE	*sys = (IOSPACE *)0x0100;
 	int	counts = 0;
+	int recdflg = 0;
     char *buf_ptr = (char *)0x800000;
 	// Let's set ourselves up for 1000000 baud, 8-bit characters, no parity,
 	// and one stop bit.
 	sys->io_uart_ctrl = 79;
-    zip_read_image(buf_ptr); 
+     
 	while(1) {
 		const char	*ptr;
 
-		ptr = msg;
+		if (recdflg == 0) ptr = msg;
 		while(*ptr) {
 			// Wait while our transmitter is busy
 			while(sys->io_uart_tx)
 				;
 			sys->io_uart_tx = *ptr++; // Transmit our character
+			recdflg = zip_read_image(buf_ptr);
+			if (recdflg == 65536) {
+				ptr = msg1;  // data has been received
+			}
 		}
 
 		// Now, wait for the top of the second
 		unsigned secv = sys->io_rtc_clock;
-		//while(secv == sys->io_rtc_clock)
-			//;
+		while(secv == sys->io_rtc_clock)
+		;
 
 		// And repeat, saying Hello, World! once each second
 
