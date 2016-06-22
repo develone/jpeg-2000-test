8495e76dbd66f3dd8e3e32eaa0e242b5dcc1ce92
vidal <vidal@vidal-MX6438.(none)>
2016-06-21 19:25:32 -0600

    The CPU should be fully loaded, you may now start it.  To start the CPU, type wbregs cpu 0 signal from zipcpu 0 waiting for hello, world! signal from zipcpu 1 sending data 1+0 records in 1+0 records out 196608 bytes (197 kB) copied, 1.97623 s, 99.5 kB/s transfer time 1.99234700203 sec waiting for Data rdy     ! transfer time 2.11466002464 sec 196608

--- zipcpu/uart_test_code/rd_rgb.py
+++ zipcpu/uart_test_code/rd_rgb.py
@@ -2,6 +2,7 @@ import serial
 import time
 import binascii
 import pigpio
+import os
 
 pi = pigpio.pi()
 zipcpu14 = 2
@@ -9,7 +10,7 @@ print "signal from zipcpu",pi.read(zipcpu14)
 ser = serial.Serial ("/dev/ttyAMA0")    
 ser.baudrate = 1000000
      
-file = open("rgb.bin", "rb")
+#file = open("rgb.bin", "rb")
 reply=[]
 print "waiting for hello, world!"
 
@@ -21,19 +22,15 @@ reply = ser.read(15)
 
 start_time = time.time()        
 print "sending data"
-for i in range(65536):
-    data = file.read(3)
-    #if(i == 0):
-    #x = binascii.b2a_hex(data)
-    #print x 
-    ser.write(data) 
+os.system("dd if=rgb.bin of=/dev/ttyAMA0 bs=196608")
+    
 end_time = time.time()
 print "transfer time", end_time-start_time, "sec"
-file.close()
+#file.close()
 start_time = time.time()
 reply=[]
 print "waiting for Data rdy     !"
-reply = ser.read(65536)
+reply = ser.read(196608)
 end_time = time.time()
 print "transfer time", end_time-start_time, "sec"
 print len(reply) 
