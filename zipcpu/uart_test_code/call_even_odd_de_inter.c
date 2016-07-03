56ab44c17f2443fbd34401277c119a023ead261d
vidal <vidal@vidal-MX6438.(none)>
2016-07-03 09:50:11 -0600

    added reading data to call_even_odd_de_inter.c Now appears to going through the image left to right top to botton.

--- zipcpu/uart_test_code/call_even_odd_de_inter.c
+++ zipcpu/uart_test_code/call_even_odd_de_inter.c
@@ -1,13 +1,98 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdint.h>
-void main(void) {
-    int w,h;
-    w = 256;
-    h = 256;
+
+ 
+#ifdef __ZIPCPU__
+void free(void *);
+typedef int int32;
+#else
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#endif
+int main(void) {
+	
+		const int bb = 0x1ff;
+    const int gg = 0x7fc00;
+    const int rr = 0x1ff00000;
+//read RGB data from file 
+ 	
+int i,ii0,ii1,ii2,val;
+int *buf;
+int xx[65536];
+buf = (int *)&xx[0];	
+struct rec
+	{
+	char raw_buf[3];
+};
+struct rec my_record;
+//struct results my_results;
+FILE *ptr_myfile;
+ptr_myfile=fopen("rgb.bin","rb");
+if (!ptr_myfile)
+{
+ 	printf("Unle to open file!");
+	return 1;
+} 
+
+for (i= 0;i<65536;i++) {
+fread(&my_record,sizeof(struct rec),1,ptr_myfile);
+ii0 = (int)my_record.raw_buf[0];
+ii1 = (int)my_record.raw_buf[1];
+ii2 = (int)my_record.raw_buf[2];
+val = ii0<<20|ii1<<10|ii2;
+*buf++=val;
+//printf("%d %x %x %x %x \n",i,ii0,ii1,ii2,val);
+
+}
+fclose(ptr_myfile);
+
+int *sptr, *sptr1, *dptr;
+
+int row,col,cc;
+int w,h,r2,r2h,red,green,blue,steps;
+w = 256;
+h = 256;
+int sar[w][h];
+int tar[w][h];
+
+sptr = (int *)malloc(sizeof(int)*(w*h));
+sptr1 = &sar[0][0]; 
+dptr = (int *)malloc(sizeof(int)*(w*h));
+//printf("%x %x %x\n",sptr,sptr1,dptr);
+cc = 0;
+
+buf = (int *)&xx[0];
+//printf("buf %x \n",buf);
+for(row=0;row<h;row++)
+{
+	for(col=0;col<w;col++)
+	{
+	    //blue = *buf&bb;
+	    //sar[row][col] = blue;
+	    red = (*buf&rr)>>20;
+		sar[row][col] = red;
+	    //green = (*buf&gg)>>10;
+		//sar[row][col] = green;		
+		//*buf = blue;
+		//printf("%d ",sar[row][col]);
+		//starts at 0 to 15
+		buf++;
+	}
+	//buf = buf + 192;
+	//printf(" %x \n",buf);
+}
+//printf("64 x 64 input data \n");
+
+	
+ 
     int *img, *alt;
     
     img = (int *)malloc(sizeof(int)*(w*h)*2);
+    for(row=0;row<h;row++) {
+    for(col=0;col<w;col++) {
+		*img++ = sar[row][col];
+    }
+    }
+    img = img - w*h;
     alt = &img[256*256];
     //printf("%d %x, %x %d\n",w,img,alt,sizeof(img)); 
 	//int	*img = SDRAM, *alt = &img[256*256];
@@ -17,5 +102,7 @@ void main(void) {
 	lifting(w, img, alt);
 	//done = 0x7fffffff - sys->io_bustimer;
 	//img[0] = done;
+	free (sptr);
+	free (dptr);
     free (img);	
     }
--- zipcpu/uart_test_code/even_odd_de_inter.c
+++ zipcpu/uart_test_code/even_odd_de_inter.c
@@ -1,50 +1,55 @@
-#include<stdio.h>
 
+#ifdef __ZIPCPU__
+void free(void *);
+typedef int int32;
+#else
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#endif
 
 void	singlelift(int rb, int w, int *ibuf, int *obuf) {
 	int	col, row,loop=0;
-    printf("%d %x, %x\n",w,ibuf,obuf);
-	for(row=0; row<rb*w; row+=rb) {
+    printf("sl %d addr %d, %d\n",w,ibuf,obuf);
+    for(col=0; col< w; col++) {
+		
+		ibuf = ibuf + col;
+		ibuf = &ibuf[col];
+		printf("col %d addr %d \n",col,ibuf);
+	for(row=1; row<w-6; row+= 2) {
 		register int	*ip, *op,*ip1,*ip2;
-		register int	c,e,bp,dp;
-		ip = ibuf+rb+row; op = obuf+rb+row;
-		ip1 = ibuf+rb*2+row;
-		ip2 = ibuf+rb*3+row;
-		// a,b,c,d,e
-		// a = a;
-		// c = c;
-		// b = (b+((a+c+2)>>2));
-		// e = e;
-		// d = (d+((c+e+2)>>2));
+		register int	a,c,e,bp,dp,idp;
+		ip = ibuf+(row*rb); op = obuf+(row*rb);
+ 
+		// dp = (dp - ((c+e)>>1));
 
-		c  = ip[0];
-		dp = ip1[0];
-		e  = ip2[0];
-		printf("row %d %x, %x %x, %d %d %d %d\n",row,&ip[0],&ip1[0],&ip2[0],(ip-ibuf),(ip1-ibuf),(ip2-ibuf),loop);
+		c  = ip[0]; 	//1 3 5 ... 245 247 249
+		dp = ip[256];	//2 4 6 ... 246 248 250
+		e  = ip[512];	//3 5 7 ... 247 249 251
+		printf("row  %d (row-1) %d sam %d  (row+1) %d \n", row, c,dp,e);
 		dp = (dp - ((c+e)>>1));
-
-		op[0] = c;
-		op[w/2] = dp;
-		printf("row %d %x \n",row,&op[w/2]);
+		printf("row %d addr (row-1) %d addr sam %d addr (row+1) %d even lift %d %d\n",row,&ip[0],&ip[256],&ip[512],dp,loop);
+		ip[256] = dp;
+		printf("row %d addr ibuf %d %d \n",row,&ip[256],ip[256]);
+		// bp = (bp+((a+c+2)>>2));
+		a = ip[-256];	//0 2 4 ... 244 246 248
+		bp = ip[0];		//1 3 5 ... 245 247 249
+		c = ip[256];	//2 4 6 ... 246 248 250 
+        printf("row %d (row-1) %d sam %d (row+1) %d \n", row, a,bp,c);
+        bp = (bp + ((a + c + 2 )>>2));
+        printf("row %d addr (row-1) %d addr sam %d addr (row+1) %d  odd lift %d %d\n",row,&ip[-256],&ip[0],&ip[256],bp,loop);
+		//op[0] = c;
+		ip[-256] = bp;
+		printf("row %d addr ibuf %d %d \n",row,&ip[-256],ip[-256]);
 		loop++;
-/*
-		for(col=0; col<w/2; col++) {
-			op+=rb; ip+=2;
-			c = e;
-			bp = dp;
-			dp = ip[1];
-			e  = ip[2];
-			printf("col %d %x, %x %x\n",col,&ip[0],&ip[1],&ip[2]);
-			op[0] = (c+((bp+dp+2)>>2)); // cp
-			dp = (dp - ((c+e)>>1));
-			op[w/2-1] = bp;
-		} op[w-1] = dp;*/
+ 
+	}
 	}
 }
 
 void	lifting(int w, int *ibuf, int *tmpbuf) {
 	int	lvl, rb=w;
-	//printf("%d %x, %x\n",w,ibuf,tmpbuf);
+	//printf("%d %d, %d\n",w,ibuf,tmpbuf);
 	for(lvl=0; lvl<3; lvl++) {
 		// Process columns -- leave result in tmpbuf
 		singlelift(rb, w, ibuf, tmpbuf);
