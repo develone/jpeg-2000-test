1b5e0f9864352a31b5c70b45ac0b2ac2de7413a8
vidal <vidal@vidal-MX6438.(none)>
2016-07-03 20:33:05 -0600

    Dan pointed out several items that were wrong with even_odd_de_inter

--- zipcpu/uart_test_code/toshare.c
+++ zipcpu/uart_test_code/toshare.c
@@ -23,13 +23,35 @@ void	entry(void) {
 }
 
 __attribute__((noinline))
-void	singlelift(int rb, int w, int *ibuf, int *obuf) {
+void	singlelift(int rb, int w, int * const ibuf, int * const obuf) {
 	int	col, row;
 
-	for(col=0; col<w; col++) {
-		register int	*ip, *op;
+	for(row=0; row<w; row++) {
+		register int	*ip, *op, *opb;
 		register int	c,e,bp,dp;
-		ip = ibuf+col; op = obuf+col;
+
+		//
+		// Ibuf walks down rows (here), but reads across columns (below)
+		// We might manage to get rid of the multiply by doing something
+		// like: 
+		//	ip = ip + (rb-w);
+		// but we'll keep it this way for now.
+		//
+		ip = ibuf+row*rb;
+
+		//
+		// Obuf walks across columns (here), writing down rows (below)
+		//
+		// Here again, we might be able to get rid of the multiply,
+		// but let's get some confidence as written first.
+		//
+		op = obuf+row;
+		opb = op + w*rb/2;
+
+		//
+		// Pre-charge our pipeline
+		//
+
 		// a,b,c,d,e
 		// a = a;
 		// c = c;
@@ -37,23 +59,29 @@ void	singlelift(int rb, int w, int *ibuf, int *obuf) {
 		// e = e;
 		// d = (d+((c+e+2)>>2));
 
+		// Put these three together so that they can get some
+		// potential pipeline savings.  (Sadly, looking at the 
+		// assembly, these could be pipelined, just the compiler
+		// didn't -- so there's some benefit to be had here.)
 		c  = ip[0];
 		dp = ip[1];
 		e  = ip[2];
 		dp = (dp + ((c+e+2)>>2));
 
-		op[0] = c;
-		op[w/2] = dp;
+		op[0]  = c;
+		opb[0] = dp;
 
-		for(row=1; row<w/2; row++) {
-			op+=rb; ip+=2;
+		for(col=1; col<w/2; col++) {
+			op +=rb; // = obuf+row+rb*col = obuf[col][row]
+			opb+=rb;// = obuf+row+rb*(col+w/2) = obuf[col+w/2][row]
+			ip+=2;	// = ibuf + (row*rb)+2*col
 			c = e;
 			bp = dp;
-			dp = ip[1];
-			e  = ip[2];
-			op[0] = (c+((bp+dp+2)>>2)); // cp
+			dp = ip[1];	// = ip[row][2*col+1]
+			e  = ip[2];	// = ip[row][2*col+2]
+			*op  = (c+((bp+dp+2)>>2)); //op[0] is obuf[col][row]
 			dp = (dp - ((c+e)>>1));
-			op[w/2-1] = bp;
+			*opb = bp;	// opb[0] is obuf[col+w/2][row-1]
 		} op[w-1] = dp;
 	}
 }
@@ -64,9 +92,26 @@ void	lifting(int w, int *ibuf, int *tmpbuf) {
 	for(lvl=0; lvl<3; lvl++) {
 		// Process columns -- leave result in tmpbuf
 		singlelift(rb, w, ibuf, tmpbuf);
+		// Process columns, what used to be the rows from the last
+		// round, pulling the data from tmpbuf and moving it back
+		// to ibuf.
 		singlelift(rb, w, tmpbuf, ibuf);
 
 		// lower_upper
+		//
+		// For this, we just adjust our pointer(s) so that the "image"
+		// we are processing, as referenced by our two pointers, now
+		// references the bottom right part of the image.
+		//
+		// Of course, we haven't really changed the dimensions of the
+		// image.  It started out rb * rb in size, or the initial w*w,
+		// we're just changing where our pointer into the image is.
+		// Rows remain rb long.  We pretend (above) that this new image
+		// is w*w, or should I say (w/2)*(w/2), but really we're just
+		// picking a new starting coordinate and it remains rb*rb.
+		//
+		// Still, this makes a subimage, within our image, containing
+		// the low order results of our processing.
 		int	offset = w*rb/2+w/2;
 		ibuf = &ibuf[offset];
 		tmpbuf = &tmpbuf[offset];
