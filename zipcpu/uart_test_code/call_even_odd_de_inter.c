8fc88a3be52f5378bfee255c02b8c20b695f89e5
vidal <vidal@vidal-MX6438.(none)>
2016-07-05 13:30:59 -0600

    adding index removed sptr sptr1 and dptr removed sar[][] tar[][]

--- zipcpu/uart_test_code/call_even_odd_de_inter.c
+++ zipcpu/uart_test_code/call_even_odd_de_inter.c
@@ -10,11 +10,11 @@ typedef int int32;
 #endif
 int main(void) {
 	
-		const int bb = 0x1ff;
+	const int bb = 0x1ff;
     const int gg = 0x7fc00;
     const int rr = 0x1ff00000;
 //read RGB data from file 
- 	
+int index; 	
 int i,ii0,ii1,ii2,val;
 int *buf;
 int xx[65536];
@@ -45,42 +45,17 @@ val = ii0<<20|ii1<<10|ii2;
 }
 fclose(ptr_myfile);
 
-int *sptr, *sptr1, *dptr;
+
 
 int row,col,cc;
 int w,h,r2,r2h,red,green,blue,steps;
 w = 256;
 h = 256;
-int sar[w][h];
-int tar[w][h];
 
-sptr = (int *)malloc(sizeof(int)*(w*h));
-sptr1 = &sar[0][0]; 
-dptr = (int *)malloc(sizeof(int)*(w*h));
-//printf("%x %x %x\n",sptr,sptr1,dptr);
+ 
 cc = 0;
 
 buf = (int *)&xx[0];
-//printf("buf %x \n",buf);
-for(row=0;row<h;row++)
-{
-	for(col=0;col<w;col++)
-	{
-	    //blue = *buf&bb;
-	    //sar[row][col] = blue;
-	    red = (*buf&rr)>>20;
-		sar[row][col] = red;
-	    //green = (*buf&gg)>>10;
-		//sar[row][col] = green;		
-		//*buf = blue;
-		//printf("%d ",sar[row][col]);
-		//starts at 0 to 15
-		buf++;
-	}
-	//buf = buf + 192;
-	//printf(" %x \n",buf);
-}
-//printf("64 x 64 input data \n");
 
 	
  
@@ -88,11 +63,10 @@ for(row=0;row<h;row++)
     
     img = (int *)malloc(sizeof(int)*(w*h)*2);
     xxx = img;
-    for(row=0;row<h;row++) {
-    for(col=0;col<w;col++) {
-		*img++ = sar[row][col];
-    }
-    }
+	for(index=0; index<h*w; index++) {
+		red = ((*buf++)&rr)>>20;
+		*img++ = red;
+	}
     img = xxx;
     alt = &img[256*256];
     //printf("%d %x, %x %d\n",w,img,alt,sizeof(img)); 
@@ -101,7 +75,7 @@ for(row=0;row<h;row++)
 
 	//sys->io_bustimer = 0x7fffffff;
 	lifting(w, img, alt);
-	
+	xxx = xxx + 57568;
 	for (row= 0 ;row<32;row++) {
 		for(col=0;col< 32;col++) {
 			printf("%d ",*xxx++);
@@ -111,7 +85,7 @@ for(row=0;row<h;row++)
 	 
 	//done = 0x7fffffff - sys->io_bustimer;
 	//img[0] = done;
-	free (sptr);
-	free (dptr);
+ 
+	
     free (img);	
     }
