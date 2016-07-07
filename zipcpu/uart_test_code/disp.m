88177f2aa3cdcc8ce49a251c04744c66cc59d70c
vidal <vidal@vidal-MX6438.(none)>
2016-07-07 06:48:17 -0600

    octave code to read dwt data sent by zipcpu and plot

---
+++ zipcpu/uart_test_code/disp.m
@@ -0,0 +1,17 @@
+%to load load "disp.m"
+%to run disp
+fid = fopen('r_dwt_zip.bin','r'); im = fread(fid, [256,inf], 'int16'); fclose(fid);
+fid = fopen('g_dwt_zip.bin','r'); im1 = fread(fid, [256,inf], 'int16'); fclose(fid);
+fid = fopen('b_dwt_zip.bin','r'); im2 = fread(fid, [256,inf], 'int16'); fclose(fid);
+figure
+imagesc(im)
+title("Red subband 3 levels dwt ZipCpu")
+colorbar
+figure
+imagesc(im1)
+title("Green subband 3 levels dwt ZipCpu")
+colorbar
+figure
+imagesc(im2)
+title("Blue subband 3 levels dwt ZipCpu")
+colorbar
