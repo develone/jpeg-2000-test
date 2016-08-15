abc89f045738d244e71428258f087ad5b3d3d7c3
vidal <vidal@vidal-MX6438.(none)>
2016-08-15 06:03:07 -0600

    used for testing test_pointer_inv.c & min_max_hist.m

--- zipcpu/uart_test_code/inv1lvl.m
+++ zipcpu/uart_test_code/inv1lvl.m
@@ -1,11 +1,15 @@
-fid = fopen('inpimg.bin','r'); im1 = fread(fid, [256,inf], 'int32'); fclose(fid);
-fid = fopen('outimg.bin','r'); im2 = fread(fid, [256,inf], 'int32'); fclose(fid);
+fid = fopen('outimg.bin','r'); im1 = fread(fid, [256,inf], 'int32'); fclose(fid);
+fid = fopen('interimg.bin','r'); im2 = fread(fid, [256,inf], 'int32'); fclose(fid);
 figure
 imagesc(im1)
 colorbar
-title "1 lvl fwd dwt "
-
+title "red subband 1 lvl fwd dwt "
+%figure
+%hist(im1)
+%title "red subband 1 lvl fwd dwt"
 figure
 imagesc(im2)
 colorbar
 title "1 lvl inv de-interleave"
+%figure
+%hist(im2)
