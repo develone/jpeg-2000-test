0c11b1de477e9038f9dec5378e7bb2e3820ac283
vidal <vidal@vidal-MX6438.(none)>
2016-09-06 20:00:37 -0600

    cleaning extra files

--- zipcpu/uart_test_code/loops64.m
+++
@@ -1,9 +0,0 @@
-fid = fopen("inpimg.bin","r"); im1 = fread(fid, [64,inf], 'int32'); fclose(fid);
-fid = fopen("lnterleave.bin","r"); im2 = fread(fid, [128,inf], 'int32'); fclose(fid);
-  
-figure
-imagesc(im1)
-colorbar
-figure
-imagesc(im2)
-colorbar
