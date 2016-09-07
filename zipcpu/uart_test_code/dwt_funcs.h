37f8aea52a6ab5ef0e86c44e8897441060f19589
vidal <vidal@vidal-MX6438.(none)>
2016-09-06 19:53:29 -0600

    cleaning up extra files

--- zipcpu/uart_test_code/dwt_funcs.h
+++
@@ -1,5 +0,0 @@
-void de_interleave(int *ptr, int w, int h);
-void lower_upper(int *ptr, int w, int h);
-void lift_step(int *ptr, int w, int h);
-void inv_lift_step(int *ptr, int w, int h);
-void upper_lower(int *ptr, int w, int h);
--- zipcpu/uart_test_code/inv3lvl.m
+++
@@ -1,5 +0,0 @@
-fid = fopen('inpimg.bin','r'); im1 = fread(fid, [256,inf], 'int32'); fclose(fid);
-figure
-imagesc(im1)
-colorbar
-title "3 lvl fwd dwt w/de-interleave"
