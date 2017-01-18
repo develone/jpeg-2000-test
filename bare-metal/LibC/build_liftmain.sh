f9626c3db5174dfab00cf500e76216a3793245b0
develone <develone@sbcglobal.net>
2017-01-18 07:26:49 -0700

    adding new files to test.c

---
+++ bare-metal/LibC/build_liftmain.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gcc -Wall -O3 -g liftmain.c lifting.c -lpng -o liftmain
---
+++ bare-metal/LibC/disp_lift.m
@@ -0,0 +1,12 @@
+fid = fopen('red-out.32t','r'); im1 = fread(fid, [512,inf], 'int32'); fclose(fid);
+fid = fopen('grn-out.32t','r'); im2 = fread(fid, [512,inf], 'int32'); fclose(fid);
+fid = fopen('blu-out.32t','r'); im3 = fread(fid, [512,inf], 'int32'); fclose(fid);
+
+figure
+imagesc(im1)
+
+figure
+imagesc(im2)
+
+figure
+imagesc(im3)
