e1f1b2d008a8367c659907c9054c0abb722eeb73
vidal <vidal@vidal-MX6438.(none)>
2017-01-21 20:51:07 -0700

    added additional comments make clean all OPT="@/home/pi/ultibo/core/fpc/bin/fpc.cfg" was chg

--- bare-metal/build_lazarus_RPi/build_lazarus.sh
+++ bare-metal/build_lazarus_RPi/build_lazarus.sh
@@ -7,7 +7,7 @@ git clone https://github.com/develone/Lazarus.git
 cp -R Lazarus lazarus
 
 cd lazarus
-
+#adding ultibo edition fpc to PATH
 export PATH=/home/pi/ultibo/core/fpc/bin:$PATH
 
 export FPCDIR=/home/pi/ultibo/core/fpc/lib/fpc/3.1.1
@@ -18,8 +18,18 @@ which fpcmake
 
 echo $FPCDIR
 
-cp jpeg-2000-test/bare-metal/build_lazarus_RPi/regexpr/Package.fpc ultibo/core/fpc/lib/fpc/3.1.1/units/arm-linux/regexpr
+#need a Package.fpc for regexpr foe ulribo edition
+#	[package]
+#	name=regexpr
+#	version=3.1.1
+#	[require]
+#	packages_linux_arm=
+cp /home/pi/jpeg-2000-test/bare-metal/build_lazarus_RPi/regexpr/Package.fpc /home/pi/ultibo/core/fpc/lib/fpc/3.1.1/units/arm-linux/regexpr
 
 fpcmake -v
 
-make 
+#make 
+
+make clean all OPT="@/home/pi/ultibo/core/fpc/bin/fpc.cfg" 
+
+#make  all OPT="@/home/pi/ultibo/core/fpc/bin/fpc.cfg"
