c2ddcddf29ec6026ea2f62c66c3866601f699b78
vidal <vidal@vidal-MX6438.(none)>
2017-01-25 17:08:44 -0700

    removes test.o & libtest.a compiles test.c --> test.o compiles test.o --> libtest.a compiles LibCTestRPi2.lpr --> kernel7.img

---
+++ bare-metal/LibC/compile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+date
+rm -f test.o
+rm -f libtest.a
+arm-none-eabi-gcc -O2 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -c test.c
+arm-none-eabi-ar rcs libtest.a test.o
+ls -la test.o libtest.a
+fpc -vi -B -Tultibo -Parm -CpARMV7A -WpRPI2B @/home/pi/ultibo/core/fpc/bin/rpi2.cfg -O2 LibCTestRPi2.lpr
