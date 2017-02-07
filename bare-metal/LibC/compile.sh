cc7383e85848b2adaece84f5bf9bcfbd54c6f071
develone <develone@sbcglobal.net>
2017-02-07 13:49:10 -0700

    This project was created from LibCTestRPi2.lpr

--- bare-metal/LibC/compile.sh
+++ bare-metal/LibC/compile.sh
@@ -6,5 +6,5 @@ rm -f kernel7.img
 arm-none-eabi-gcc -O2 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -c test.c
 arm-none-eabi-ar rcs libtest.a test.o
 
-fpc -vi -B -Tultibo -Parm -CpARMV7A -WpRPI2B @/home/pi/ultibo/core/fpc/bin/rpi2.cfg -O2 LibCTestRPi2.lpr
+fpc -vi -B -Tultibo -Parm -CpARMV7A -WpRPI2B @/home/pi/ultibo/core/fpc/bin/rpi2.cfg -O2 LibCTest_RPi2.lpr
 ls -la test.o libtest.a kernel7.img
