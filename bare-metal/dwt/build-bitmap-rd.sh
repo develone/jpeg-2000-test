96c92d4905e9b4545523f92075cb3ee74c668f9d
vidal <vidal@vidal-MX6438.(none)>
2017-03-03 11:12:32 -0700

    test 03/03/17

--- bare-metal/dwt/build-bitmap-rd.sh
+++ bare-metal/dwt/build-bitmap-rd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  
-gcc -g -c mct.c -lm -o mct.o
-gcc -g -c dwt.c -lm -o dwt.o
-gcc -g -c thread.c -lm -o thread.o
-gcc -g -c opj_malloc.c -lm -o opj_malloc.o
-gcc -g bitmap-rd.c opj_malloc.o mct.o dwt.o thread.o -lm -o bitmap-rd
+#gcc -g -c mct.c -lm -o mct.o
+#gcc -g -c dwt.c -lm -o dwt.o
+#gcc -g -c thread.c -lm -o thread.o
+#gcc -g -c opj_malloc.c -lm -o opj_malloc.o
+gcc  -g bitmap-rd.c -L. -lopenjp2 -lm -o bitmap-rd
--- bare-metal/dwt/compile.sh
+++ bare-metal/dwt/compile.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 date
-rm -f mct.o
-rm -f opj_malloc.o
+
 rm -f dwtlift.o
 rm -f libdwtlift.a
 rm -f kernel7.img
-arm-none-eabi-gcc -O2 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -c opj_malloc.c
-arm-none-eabi-gcc -O2 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -c mct.c
-arm-none-eabi-gcc -O2 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -c dwtlift.c
-arm-none-eabi-ar rcs libdwtlift.a dwtlift.o mct.o opj_malloc.o
 
+
+arm-none-eabi-gcc -O2 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -c dwtlift.c 
+#The file was created using the script build_gccultibo.sh
+#src/lib/openjp2 folder 
+#the files opj_malloc.c dwt.c
+arm-none-eabi-ar rcs libopenjp2.a dwtlift.o
+cp libopenjp2.a libdwtlift.a
 fpc -vi -B -Tultibo -Parm -CpARMV7A -WpRPI2B @/home/pi/ultibo/core/fpc/bin/rpi2.cfg -O2 DWT_LIFT_RPi2.lpr
 ls -la dwtlift.o libdwtlift.a kernel7.img
+echo "when ./compile.sh is executed should be 21"
+echo "Word cpunt libopenjp2_obj.txt in src/lib/openjp2"
+wc libopenjp2_obj.txt
