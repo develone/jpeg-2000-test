62b64a13bdae16566d214902dfcca44a9b4b0c05
Edward Vidal Jr <develone@sbcglobal.net>
2017-03-23 18:29:32 +0000

    fix error in title of disp_rgb.m adding images for documentation

--- bare-metal/openjp/disp_rgb.m
+++ bare-metal/openjp/disp_rgb.m
@@ -1,11 +1,11 @@
 fid = fopen('red-out.32t','r'); im1 = fread(fid, [1024,inf], 'char'); fclose(fid);
-figure; imagesc(im1); colorbar; colormap 'gray';title"red sub band 1024 x 1924 RPi bare-metal ultibo "
+figure; imagesc(im1); colorbar; colormap 'gray';title"red sub band 1024 x 1024 RPi bare-metal ultibo "
 
 fid = fopen('grn-out.32t','r'); im2 = fread(fid, [1024,inf], 'char'); fclose(fid);
-figure; imagesc(im2); colorbar; colormap 'gray';title"green sub band 1024 x 1924 RPi bare-metal ultibo "
+figure; imagesc(im2); colorbar; colormap 'gray';title"green sub band 1024 x 1024 RPi bare-metal ultibo "
 
 fid = fopen('blu-out.32t','r'); im3 = fread(fid, [1024,inf], 'char'); fclose(fid);
-figure; imagesc(im3); colorbar; colormap 'gray';title"blue sub band 1024 x 1924 RPi bare-metal ultibo "
+figure; imagesc(im3); colorbar; colormap 'gray';title"blue sub band 1024 x 1024 RPi bare-metal ultibo "
 
 fid = fopen('dwt-out.32t','r'); im4 = fread(fid, [1024,inf], 'int32'); fclose(fid);
-figure; imagesc(im4); colorbar; colormap 'gray';title" 1024 x 1924 n = 5 lvls numresolution 4 RPi bare-metal ultibo "
+figure; imagesc(im4); colorbar; colormap 'gray';title" 1024 x 1024 n = 5 lvls numresolution 4 RPi bare-metal ultibo "
