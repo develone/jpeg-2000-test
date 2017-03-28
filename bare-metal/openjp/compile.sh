e9c9b978b22fc5cfbae4b0c72f6664c0479bbcd1
Edward Vidal Jr <develone@sbcglobal.net>
2017-03-28 15:52:52 +0000

    cleaning up errors in compile.sh since the adding of openjpeg.o to libopenjp2.a

--- bare-metal/openjp/compile.sh
+++ bare-metal/openjp/compile.sh
@@ -7,10 +7,12 @@ rm -f kernel7.img
 rm -f libopenjp2.a
 
 cp ~/t_ultibo/src/lib/openjp2/libopenjp2.a .
+echo "Word count arm-none-eabi-ar -t libopenjp2.a copied from src/lib/openjp2"
 arm-none-eabi-ar -t libopenjp2.a | wc
 arm-none-eabi-objdump -d libopenjp2.a > dis_orig_libopenjp2.txt
+
 arm-none-eabi-gcc -L. -llibopenjp2 -O2 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -c dwtlift.c 
-#The file was created using the script build_gccultibo.sh
+#The file was created using the script compile_ultibo.sh
 #src/lib/openjp2 folder 
 #the files opj_malloc.c dwt.c
 arm-none-eabi-ar rcs libopenjp2.a dwtlift.o 
@@ -19,6 +21,6 @@ arm-none-eabi-ar -t libopenjp2.a > libopenjp2_obj.txt
 arm-none-eabi-objdump -d libopenjp2.a > dis_libopenjp2.txt
 fpc -vi -B -Tultibo -Parm -CpARMV7A -WpRPI2B @/home/pi/ultibo/core/fpc/bin/rpi2.cfg -O2 DWT_LIFT_RPi2.lpr
 ls -la dwtlift.o libdwtlift.a kernel7.img libopenjp2.a
-echo "when ./compile.sh is executed should be 21"
+echo "when ./compile.sh is executed should be 23"
 echo "Word count libopenjp2_obj.txt in src/lib/openjp2"
 wc libopenjp2_obj.txt
